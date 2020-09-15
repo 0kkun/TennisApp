136be08d3ece83c8cc8a24048498ab9bcae0e7cc
0kkun <shinji.okumoto.01@gmail.com>
2020-09-16 02:56:22 +0900

    バルクインサートトレイトの実装

--- app/models/Player.php
+++ app/models/Player.php
@@ -6,7 +6,32 @@ use Illuminate\Database\Eloquent\Model;
 
 class Player extends Model
 {
-    protected $table = 'players';
+    use BulkInsertOrUpdateTrait;
 
-    protected $fillable = ['name_jp','name_en','wiki_url','country','age',];
+    protected $table = 'players';
+    protected $guarded = [];
+    protected $fillable = [
+        'name_jp',
+        'name_en',
+        'wiki_url',
+        'country',
+        'age'
+    ];
+
+    /**
+     * バルクインサート時のカラム指定
+     *
+     * @return array
+     */
+    protected function getUpdateColumnsOnDuplicate(): array
+    {
+        return [
+            'name_jp',
+            'name_en',
+            'wiki_url',
+            'country',
+            'age',
+            'updated_at',
+        ];
+    }
 }
