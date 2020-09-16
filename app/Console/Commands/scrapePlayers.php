901d8d93c74a45de733b250b4b4d7e5935baf2af
0kkun <shinji.okumoto.01@gmail.com>
2020-09-17 03:00:11 +0900

    scrapePlayersのリファクタ(振舞いごとにメソッド分割)

--- app/Console/Commands/scrapePlayers.php
+++ app/Console/Commands/scrapePlayers.php
@@ -25,7 +25,7 @@ class scrapePlayers extends Command
      *
      * @var string
      */
-    protected $description = '[Player] Player nameをwikiからスクレイピングで取得するコマンド';
+    protected $description = '[Player] 選手の情報をwikiからスクレイピングで取得するコマンド';
 
     /**
      * Create a new command instance.
@@ -48,95 +48,47 @@ class scrapePlayers extends Command
     public function handle()
     {
         $this->info("実行開始");
-        $this->logger = new BatchLogger('scrapePlayers');
-        $origin_data = array(); // 全テキスト取得用
+        $this->logger = new BatchLogger( 'scrapePlayers' );
 
         try {
-            $this->logger->write('取得開始', 'info', true);
+            $this->logger->write( '取得開始', 'info', true );
 
-            $goutte = GoutteFacade::request('GET', 'https://ja.wikipedia.org/wiki/男子テニス選手一覧');
+            $goutte = GoutteFacade::request( 'GET', 'https://ja.wikipedia.org/wiki/男子テニス選手一覧' );
             // アクセスしたら一旦sleep
             sleep(1);
 
             // 全てのテキスト取得
-            $goutte->filter('ul li')->each(function ($node) use (&$origin_data) {
-                // ノードが空の時のエラーハンドリング
-                if ($node->count() > 0) {
-                    // ここでは配列が返されるのでarray_pushが必要
-                    array_push( $origin_data, $node->text() );
-                } else {
-                    $this->info("スクレイピング実行できませんでした。");
-                }
-            });
+            $origin_data = $this->scrapePlayersAllText( $goutte );
 
-            $all_data = $this->excludeOtherInfo($origin_data);
+            // 不必要なテキストを削除
+            $all_data = $this->excludeOtherInfo( $origin_data );
 
-            $count = count($all_data);
+             // 日本語の名前だけを抽出
+            $player_data['name_jp'] = $this->abstractPlayersNameJp( $all_data );
 
-            // 日本語の名前だけを抽出
-            for ($i=0; $i<$count; $i++){
-                if ( preg_match('@^(?:)?([^ ]+)@i', $all_data[$i], $matches) ) {
-                    $name_jp[$i] = $matches[0];
-                } else {
-                    $name_jp[$i] = null;
-                }
-            }
+            // 英語の名前だけを抽出
+            $player_data['name_en'] = $this->abstractPlayersNameEn( $all_data );
 
-            // 英語の名前だけを抽出。"("または"（"から始まり、大文字小文字含めた英語で直前の0文字以上にマッチするもの。
-            for ($i=0; $i<$count; $i++){
-                if ( preg_match('/([^(|（])+([A-Za-z])+/u', $all_data[$i], $matches) ) {
-                    $name_en[$i] = $matches[0];
-                } else {
-                    $name_en[$i] = null;
-                }
-            }
+            // 出身国名を抽出
+            $player_data['country'] = $this->abstractPlayersCountry( $all_data );
 
-            // 出身国名を抽出。全角括弧で囲まれている文字を抽出する
-            for ($i=0; $i<$count; $i++){
-                if (preg_match('/(?<=[（]).*?(?=[）])/u', $all_data[$i], $matches)) {
-                    $country[$i] = $matches[0];
-                } else {
-                    $country[$i] = null;
-                }
-            }
+            // 日本語名を使ってurlを作成し、選手の年齢をスクレイピング
+            $detail_data = $this->scrapePlayersAgeAndUrl( $player_data['name_jp'] );
+            $player_data['age'] = $detail_data['age'];
+            $player_data['wiki_url'] = $detail_data['wiki_url'];
 
-            // 年齢抽出。選手のリンク先で取得する
-            for ( $i=0; $i<$count; $i++) {
-                $wiki_url[$i] = 'https://ja.wikipedia.org//wiki/' . $name_jp[$i];
-                $goutte_detail = GoutteFacade::request('GET', $wiki_url[$i]);
-                sleep(0.5);
-
-                if($goutte_detail->filter('.infobox')->count() > 0) {
-                    $side_bar_text = $goutte_detail->filter('.infobox')->text();
-                    // 文字列の中から2桁の数値であり、かつ"歳"の直前である物を抽出
-                    $age[$i] = $this->extract_age($side_bar_text);
-                    $this->info($i . ':' . $age[$i] . '歳');
-                } else {
-                    $age[$i] = null;
-                }
-            }
-
-            $today = Carbon::now();
-
-            for ($i=0; $i<$count; $i++) {
-                $data[$i] = [
-                    'name_jp'    => $name_jp[$i],
-                    'name_en'    => $name_en[$i],
-                    'country'    => $country[$i],
-                    'wiki_url'   => $wiki_url[$i],
-                    'age'        => $age[$i],
-                    'created_at' => $today,
-                    'updated_at' => $today,
-                ];
-            }
+            // 保存用に加工。日付情報も付加。
+            $value = $this->makeInsertValue($player_data);
 
             $this->logger->write('取得完了', 'info' ,true);
+            $this->info("取得完了");
 
             // バルクインサートで保存
-            if (!empty($data)) {
-                $this->players_repository->bulkInsertOrUpdate($data);
+            if (!empty($value)) {
+                $this->players_repository->bulkInsertOrUpdate($value);
             }
 
+            $this->info("保存完了");
             $this->logger->write('保存完了', 'info' ,true);
             $this->logger->success();
 
@@ -153,9 +105,9 @@ class scrapePlayers extends Command
      * 特定の不必要なワードを除外する
      *
      * @param array $data
-     * @return Array
+     * @return array
      */
-    private function excludeOtherInfo( array $data ): Array
+    private function excludeOtherInfo( array $data ): array
     {
         $exclude_words = array(
             "","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
@@ -181,23 +133,154 @@ class scrapePlayers extends Command
 
 
     /**
+     * 全てのテキストをスクレイピングで取得
+     *
+     * @param mixed $goutte
+     * @return array
+     */
+    private function scrapePlayersAllText( $goutte ): array
+    {
+        $origin_data = array();
+
+        // 全てのテキスト取得
+        $goutte->filter('ul li')->each(function ($node) use (&$origin_data) {
+            // ノードが空の時のエラーハンドリング
+            if ( $node->count() > 0 ) {
+                // ここでは配列が返されるのでarray_pushが必要
+                array_push( $origin_data, $node->text() );
+            } else {
+                $this->info("スクレイピング実行できませんでした。");
+            }
+        });
+
+        return $origin_data;
+    }
+
+
+    /**
+     * 正規表現で選手の日本語名だけを抽出
+     *
+     * @param array $all_data
+     * @return array
+     */
+    private function abstractPlayersNameJp(array $all_data): array
+    {
+        $count = count( $all_data );
+        $pattern = '@^(?:)?([^ ]+)@i';
+
+        for ( $i=0; $i < $count; $i++ ){
+            if ( preg_match( $pattern, $all_data[$i], $matches )) {
+                $name_jp[$i] = $matches[0];
+            } else {
+                $name_jp[$i] = null;
+            }
+        }
+
+        return $name_jp;
+    }
+
+
+    /**
+     * 正規表現で選手の英語名だけを抽出
+     *
+     * @param array $all_data
+     * @return array
+     */
+    private function abstractPlayersNameEn( array $all_data ): array
+    {
+        $count = count( $all_data );
+        $pattern = '/([^(|（])+([A-Za-z])+/u';
+
+        // 英語の名前だけを抽出。"("または"（"から始まり、大文字小文字含めた英語で直前の0文字以上にマッチするもの。
+        for ( $i=0; $i < $count; $i++ ){
+            if ( preg_match( $pattern, $all_data[$i], $matches )) {
+                $name_en[$i] = $matches[0];
+            } else {
+                $name_en[$i] = null;
+            }
+        }
+
+        return $name_en;
+    }
+
+
+    /**
+     * 正規表現で選手の出身国だけを抽出
+     *
+     * @param array $all_data
+     * @return array
+     */
+    private function abstractPlayersCountry( array $all_data ): array
+    {
+        $count = count( $all_data );
+        $pattern = '/(?<=[（]).*?(?=[）])/u';
+
+        // 出身国名を抽出。全角括弧で囲まれている文字を抽出する
+        for ( $i=0; $i < $count; $i++ ){
+            if ( preg_match( $pattern, $all_data[$i], $matches )) {
+                $country[$i] = $matches[0];
+            } else {
+                $country[$i] = null;
+            }
+        }
+
+        return $country;
+    }
+
+
+    /**
+     * 選手の名前を使ってurlを作成し、年齢をスクレイピング
+     *
+     * @param array $name_jp
+     * @return array
+     */
+    private function scrapePlayersAgeAndUrl( array $name_jp ): array
+    {
+        $count = count( $name_jp );
+
+        for ( $i=0; $i<$count; $i++) {
+            $wiki_url[$i] = 'https://ja.wikipedia.org//wiki/' . $name_jp[$i];
+            $goutte_detail = GoutteFacade::request('GET', $wiki_url[$i]);
+            sleep(0.5);
+
+            if( $goutte_detail->filter('.infobox')->count() > 0 ) {
+                // サイドボックスのテキストを取得
+                $side_bar_text = $goutte_detail->filter('.infobox')->text();
+
+                // 文字列の中から2桁の数値であり、かつ"歳"の直前である物を抽出
+                $age[$i] = $this->extractAge( $side_bar_text );
+
+                $this->info( $i . $name_jp[$i] . ':' . $age[$i] . '歳' );
+            } else {
+                $age[$i] = null;
+            }
+        }
+
+        return [
+            'age' => $age,
+            'wiki_url' => $wiki_url
+            ];
+    }
+
+
+    /**
      * 年齢のみ抽出する
      *
-     * @param string ソース文字列
-     * @return 抽出した数値
+     * @param string $subject (ソース文字列)
+     * @return 抽出した数値 or null
      */
-    function extract_age( string $subject )
+    function extractAge( string $subject )
     {
 
         $pattern1 = '/[[0-9]{2}歳/u';
 
-        if (!preg_match($pattern1, $subject, $text)) {
+        if ( !preg_match( $pattern1, $subject, $text ) ) {
             return null;
         }
 
-        $pattern = '/[0-9.,０-９．，]+/u'; // パターン。末尾のuを忘れずに。
+        $pattern = '/[0-9.,０-９．，]+/u';
 
-        if ( $result = preg_match( $pattern, $text[0], $matches ) ) {
+        if ( preg_match( $pattern, $text[0], $matches ) ) {
             // マッチングした数字を抜き出す
             $num = $matches[0];
             // 半角数字に変換
@@ -207,6 +290,7 @@ class scrapePlayers extends Command
             // 小数の値として正規化
             $num_int = (int) $num_plain;
 
+            // 15歳以下は存在しないはずだが一応除く
             if ( $num_int <= 15 ) {
                 $num_int = null;
             }
@@ -215,5 +299,33 @@ class scrapePlayers extends Command
         } else {
             return null;
         }
+    }
+
+
+    /**
+     * レコード保存用のデータを作成。
+     * 日付情報を付加
+     *
+     * @param array $player_data
+     * @return array
+     */
+    private function makeInsertValue( array $player_data ): array
+    {
+        $count = count( $player_data['name_jp'] );
+        $today = Carbon::now();
+
+        for ( $i=0; $i < $count; $i++ ) {
+            $value[$i] = [
+                'name_jp'    => $player_data['name_jp'][$i],
+                'name_en'    => $player_data['name_en'][$i],
+                'country'    => $player_data['country'][$i],
+                'age'        => $player_data['age'][$i],
+                'wiki_url'   => $player_data['wiki_url'][$i],
+                'created_at' => $today,
+                'updated_at' => $today
+            ];
+        }
+
+        return $value;
     }
 }
