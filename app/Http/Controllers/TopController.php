0864cd19e4757173139628ee438d0f09281974c0
0kkun <shinji.okumoto.01@gmail.com>
2020-09-29 07:49:21 +0900

    コントローラに作成したサービスの使用記述

--- app/Http/Controllers/TopController.php
+++ app/Http/Controllers/TopController.php
@@ -6,11 +6,15 @@ use Illuminate\Http\Request;
 use App\Repositories\Contracts\PlayersRepository;
 use App\Repositories\Contracts\AtpRankingsRepository;
 use App\Repositories\Contracts\NewsArticlesRepository;
+use App\Repositories\Contracts\FavoritePlayersRepository;
+use App\Services\Top\TopServiceInterface;
 
 class TopController extends Controller
 {
     private $atp_rankings_repository;
     private $news_articles_repository;
+    private $favorite_players_repository;
+    private $top_service;
 
     /**
      * リポジトリをDI
@@ -19,12 +23,16 @@ class TopController extends Controller
      */
     public function __construct(
         AtpRankingsRepository $atp_rankings_repository,
-        NewsArticlesRepository $news_articles_repository
+        NewsArticlesRepository $news_articles_repository,
+        FavoritePlayersRepository $favorite_players_repository,
+        TopServiceInterface $top_service
 
     )
     {
         $this->atp_rankings_repository = $atp_rankings_repository;
         $this->news_articles_repository = $news_articles_repository;
+        $this->favorite_players_repository = $favorite_players_repository;
+        $this->top_service = $top_service;
     }
 
 
@@ -36,8 +44,11 @@ class TopController extends Controller
     public function index()
     {
         $atp_rankings = $this->atp_rankings_repository->getAll()->toArray();
-        $news_articles = $this->news_articles_repository->getAll();
+
+        $news_articles = $this->top_service->getArticleByFavoritePlayer();
 
         return view('top.index', compact('atp_rankings', 'news_articles'));
     }
+
+
 }
