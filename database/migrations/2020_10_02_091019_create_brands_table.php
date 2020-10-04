b3c12d91de2807e446c9cb7d9b6a1b9f96f09aca
0kkun <shinji.okumoto.01@gmail.com>
2020-10-04 11:54:20 +0900

    brandsテーブル作成

---
+++ database/migrations/2020_10_02_091019_create_brands_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBrandsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('brands', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name_jp');
+            $table->string('name_en');
+            $table->string('country');
+
+            $table->unique('name_jp');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('brands');
+    }
+}
