0e187f1a9c892530afab59743f3091622806c6dc
0kkun <shinji.okumoto.01@gmail.com>
2020-10-06 00:04:53 +0900

    ビューにお気に入りに基づくブランドニュース表示

--- resources/views/Top/contents-left.blade.php
+++ resources/views/Top/contents-left.blade.php
@@ -6,15 +6,29 @@
         @foreach ( $news_articles as $index => $news_article )
           @if ( $index % 2 === 0 )
             @if ( $news_article['post_time'] == $today->format('Y-m-d') )
-              <li class="top-left-li pl-1 bg-gray font-weight-bold"><a href="{{ $news_article['url'] }}" id="open">{{ $news_article['title'] }} - {{$news_article['post_time']}} <span class="text-danger">new!</span></a></li>
+              <li class="top-left-li pl-1 bg-gray">
+                <a class="font-weight-bold" href="{{ $news_article['url'] }}">{{ $news_article['title'] }}</a>
+                <span>- {{$news_article['post_time']}}</span>
+                <span class="text-danger font-weight-bold">new!</span>
+            </li>
             @else
-              <li class="top-left-li pl-1 bg-gray"><a href="{{ $news_article['url'] }}" id="open">{{ $news_article['title'] }} - {{$news_article['post_time']}}</a></li>
+              <li class="top-left-li pl-1 bg-gray">
+                <a href="{{ $news_article['url'] }}">{{ $news_article['title'] }}</a>
+                <span>- {{$news_article['post_time']}}</span>
+              </li>
             @endif
           @else
             @if ( $news_article['post_time'] == $today->format('Y-m-d') )
-              <li class="top-left-li pl-1 bg-light font-weight-bold"><a href="{{ $news_article['url'] }}" id="open">{{ $news_article['title'] }} - {{$news_article['post_time']}} <span class="text-danger">new!</span></a></li>
+              <li class="top-left-li pl-1 bg-light">
+                <a class="font-weight-bold" href="{{ $news_article['url'] }}">{{ $news_article['title'] }}</a>
+                <span>- {{$news_article['post_time']}}</span>
+                <span class="text-danger font-weight-bold">new!</span>
+              </li>
             @else
-              <li class="top-left-li pl-1 bg-light"><a href="{{ $news_article['url'] }}" id="open">{{ $news_article['title'] }} - {{$news_article['post_time']}}</a></li>
+              <li class="top-left-li pl-1 bg-light">
+                <a href="{{ $news_article['url'] }}">{{ $news_article['title'] }}</a>
+                <span>- {{$news_article['post_time']}}</span>
+              </li>
             @endif
           @endif
         @endforeach
@@ -30,20 +44,47 @@
 <div class="top-contents-left mt-3">
   <div class="top-contents-head text-center bg-dark text-white h4 font-alegreya">Item News</div>
   <div class="top-left-list-box">
-    <ul>
-      <li class="top-left-li">≫ <a href="#">News 1</a></li>
-      <li class="top-left-li">≫ <a href="#">News 1</a></li>
-      <li class="top-left-li">≫ <a href="#">News 1</a></li>
-      <li class="top-left-li">≫ <a href="#">News 1</a></li>
-      <li class="top-left-li">≫ <a href="#">News 1</a></li>
-      <li class="top-left-li">≫ <a href="#">News 1</a></li>
-      <li class="top-left-li">≫ <a href="#">News 1</a></li>
-      <li class="top-left-li">≫ <a href="#">News 1</a></li>
-      <li class="top-left-li">≫ <a href="#">News 1</a></li>
-      <li class="top-left-li">≫ <a href="#">News 1</a></li>
-      <li class="top-left-li">≫ <a href="#">News 1</a></li>
-      <li class="top-left-li">≫ <a href="#">News 1</a></li>
-    </ul>
+    @if ( !empty($brand_news_articles) )
+      <ul>
+        @foreach ( $brand_news_articles as $index => $brand_news_article )
+          @if ( $index % 2 === 0 )
+            @if ( $brand_news_article['post_time'] == $today->format('Y-m-d') )
+              <li class="top-left-li pl-1 bg-gray">
+                <span class="bg-success rounded font-14 text-white m-1">{{ $brand_news_article['brand_name'] }}</span>
+                <a class="font-weight-bold" href="{{ $brand_news_article['url'] }}">{{ $brand_news_article['title'] }} </a>
+                <span> - {{$brand_news_article['post_time']}}</span>
+                <span class="text-danger">new!</span>
+              </li>
+            @else
+              <li class="top-left-li pl-1 bg-gray">
+                <span class="bg-success rounded font-14 text-white m-1">{{ $brand_news_article['brand_name'] }}</span>
+                <a href="{{ $brand_news_article['url'] }}">{{ $brand_news_article['title'] }}</a>
+                <span> - {{$brand_news_article['post_time']}}</span>
+              </li>
+            @endif
+          @else
+            @if ( $brand_news_article['post_time'] == $today->format('Y-m-d') )
+              <li class="top-left-li pl-1 bg-light">
+                <span class="bg-success rounded font-14 text-white m-1">{{ $brand_news_article['brand_name'] }}</span>
+                <a class="font-weight-bold" href="{{ $brand_news_article['url'] }}">{{ $brand_news_article['title'] }}</a>
+                <span> - {{$brand_news_article['post_time']}}</span>
+                <span class="text-danger font-weight-bold">new!</span>
+              </li>
+            @else
+              <li class="top-left-li pl-1 bg-light">
+                <span class="bg-success rounded font-14 text-white m-1">{{ $brand_news_article['brand_name'] }}</span>
+                <a href="{{ $brand_news_article['url'] }}">{{ $brand_news_article['title'] }}</a>
+                <span> - {{$brand_news_article['post_time']}}</span>
+              </li>
+            @endif
+          @endif
+        @endforeach
+      </ul>
+      <!-- ページネーション -->
+      <div class="p-3 text-center">
+        {{ $brand_news_articles->appends((request()->query()))->links() }}
+      </div>
+    @endif
   </div>
 </div>
 
