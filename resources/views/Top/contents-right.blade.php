8408597b449026f7bf2f7804e04242102eab4d0a
0kkun <shinji.okumoto.01@gmail.com>
2020-10-15 08:33:58 +0900

    前回ランクとの比較矢印表示を追加

--- resources/views/Top/contents-right.blade.php
+++ resources/views/Top/contents-right.blade.php
@@ -17,7 +17,15 @@
           @if ( !empty($atp_rankings) )
             @foreach ( $atp_rankings as $ranking )
               <tr>
-                <td class="top-contents-right-td top-right-table-rank-w text-center">{{ $ranking['rank'] }}</td>
+                @if ( $ranking['rank'] < $ranking['pre_rank']  )
+                  <td class="top-contents-right-td top-right-table-rank-w text-center">{{ $ranking['rank'] }} <i class="fas fa-arrow-up rank-up-arrow"></i></td>
+                @elseif ( $ranking['rank'] > $ranking['pre_rank'] )
+                  <td class="top-contents-right-td top-right-table-rank-w text-center">{{ $ranking['rank'] }} <i class="fas fa-arrow-down rank-down-arrow"></i></td>
+                @elseif ( $ranking['rank'] == $ranking['pre_rank'] )
+                  <td class="top-contents-right-td top-right-table-rank-w text-center">{{ $ranking['rank'] }} <i class="fas fa-arrow-right rank-right-arrow"></i></td>
+                @else
+                  <td class="top-contents-right-td top-right-table-rank-w text-center">{{ $ranking['rank'] }}</td>
+                @endif
                 <td class="top-contents-right-td top-right-table-name-w text-center">{{ $ranking['name'] }}</td>
                 <td class="top-contents-right-td top-right-table-point-w text-right">{{ $ranking['point'] }}pt</td>
               </tr>
