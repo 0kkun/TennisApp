4c5bd8f05941e3941b9e8c55f62c60d23ee1deb6
0kkun <shinji.okumoto.01@gmail.com>
2020-10-31 01:54:19 +0900

    datepickerを導入

---
+++ public/js/common/datepick.js
@@ -0,0 +1,12 @@
+$(function(){
+  $("#datepicker_start").datepicker({
+    dateFormat: 'yy-mm-dd',
+    changeYear: true,
+    changeMonth: true,
+  });
+  $("#datepicker_end").datepicker({
+    dateFormat: 'yy-mm-dd',
+    changeYear: true,
+    changeMonth: true,
+  });
+});
