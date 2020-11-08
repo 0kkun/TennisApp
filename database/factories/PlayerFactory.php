afce912551150f901eaf4d99ea0483cc90a58460
0kkun <shinji.okumoto.01@gmail.com>
2020-11-08 22:58:28 +0900

    PlayerFactory編集

--- database/factories/PlayerFactory.php
+++ database/factories/PlayerFactory.php
@@ -5,9 +5,13 @@ use App\Models\Player;
 
 $factory->define(Player::class, function (Faker $faker) {
     return [
-        'name_jp' => $faker->name(),
-        'name_en' => $faker->name(),
-        
-
+        'name_jp'        => $faker->name(),
+        'name_en'        => $faker->name(),
+        'country'        => $faker->country(),
+        'wiki_url'       => $faker->url(),
+        'age'            => $faker->randomNumber(50),
+        'created_at'     => now(),
+        'updated_at'     => now(),
+        'youtube_active' => 0
     ];
 });
