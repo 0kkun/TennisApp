b8607cc16488d1b7c416e092d98dcdd79732327f
0kkun <shinji.okumoto.01@gmail.com>
2020-11-11 02:42:54 +0900

    ビューのインデント修正

--- resources/views/favorite_player/index.blade.php
+++ resources/views/favorite_player/index.blade.php
@@ -1,172 +1,166 @@
 @extends('layouts.layout')
 
 @section('content')
-  @if(Auth::check())
+    @if(Auth::check())
 
-    <!---------------- モーダル用 ------------------->
-    <div id="mask" class="hidden"></div>
-    <!-- モーダル画面 -->
-    <div id="modal" class="hidden">
-      <div class="modal-conteiner">
-        <div class="modal-head">
-          <div class="h3 pl-2">Favorite Player機能の使い方</div>
-          <button class="btn btn-danger text-right" id="close">X</button>
+        <!---------------- モーダル用 ------------------->
+        <div id="mask" class="hidden"></div>
+        <!-- モーダル画面 -->
+        <div id="modal" class="hidden">
+            <div class="modal-conteiner">
+                <div class="modal-head">
+                    <div class="h3 pl-2">Favorite Player機能の使い方</div>
+                    <button class="btn btn-danger text-right" id="close">X</button>
+                </div>
+                <div class="modal-body">
+                    <p>お気に入り選手を検索し、登録することでトップページで表示する情報を絞り込むことができます。</p>
+                </div>
+            </div>
         </div>
-        <div class="modal-body">
-          <p>お気に入り選手を検索し、登録することでトップページで表示する情報を絞り込むことができます。</p>
-        </div>
-      </div>
-    </div>
-    <!--------------------------------------------->
+        <!--------------------------------------------->
 
-    <div class="favorite-player-wrapper container-fluid pt-140">
+        <div class="favorite-player-wrapper container-fluid pt-140">
 
-
-
-      <div class="row">
-
-        <div class="col-sm-5 pt-5">
-          <div class="form-group p-4 h4 bg-light rounded" style="height:300px;">
-            <div class="font-alegreya h4 pb-2">Search Player<a class="pl-3 pt-1 fas fa-question-circle question-btn" id="open"></a></div>
             <div class="row">
+                <div class="col-sm-5 pt-5">
+                    <div class="form-group p-4 h4 bg-light rounded" style="height:300px;">
+                        <div class="font-alegreya h4 pb-2">Search Player<a class="pl-3 pt-1 fas fa-question-circle question-btn" id="open"></a></div>
+                        <div class="row">
 
-              <div class="col-3">
-                <div class="text-left font-16 p-2" style="line-height:24px;">Name:</div>
-                <div class="text-left font-16 p-2" style="line-height:24px;">Country:</div>
-                <div class="text-left font-16 p-2" style="line-height:24px;">Age:</div>
-              </div>
+                            <div class="col-3">
+                                <div class="text-left font-16 p-2" style="line-height:24px;">Name:</div>
+                                <div class="text-left font-16 p-2" style="line-height:24px;">Country:</div>
+                                <div class="text-left font-16 p-2" style="line-height:24px;">Age:</div>
+                            </div>
 
-              <div class="col-9">
-                <form action="{{ route( 'favorite_player.index') }}" method="GET">
-                  @csrf
-                  <input class="form-control mb-1" type="text" name="name" value="{{ $params['name'] ?? '' }}" plceholder="Please input keywords...">
+                            <div class="col-9">
+                                <form action="{{ route( 'favorite_player.index') }}" method="GET">
+                                    @csrf
+                                    <input class="form-control mb-1" type="text" name="name" value="{{ $params['name'] ?? '' }}" plceholder="Please input keywords...">
 
-                  <select class="form-control mb-1" name="country">
-                    @if ( empty($params['country']) )
-                      <option value=""> All </option>
-                      @foreach ( $country_names as $key => $country )
-                        <option value="{{ $country }}"> {{ $country }} </option>
-                      @endforeach
-                    @else
-                      <option value="{{ $params['country'] }}"> {{ $params['country']}} </option>
-                      <option value=""> All </option>
-                      @foreach ( $country_names as $key => $country )
-                        <option value="{{ $country }}"> {{ $country }} </option>
-                      @endforeach
-                    @endif
-                  </select>
+                                    <select class="form-control mb-1" name="country">
+                                        @if ( empty($params['country']) )
+                                            <option value=""> All </option>
+                                        @foreach ( $country_names as $key => $country )
+                                            <option value="{{ $country }}"> {{ $country }} </option>
+                                        @endforeach
+                                        @else
+                                            <option value="{{ $params['country'] }}"> {{ $params['country']}} </option>
+                                            <option value=""> All </option>
+                                        @foreach ( $country_names as $key => $country )
+                                            <option value="{{ $country }}"> {{ $country }} </option>
+                                        @endforeach
+                                        @endif
+                                    </select>
 
-                  <select class="form-control mb-1" name="age">
-                      @if ( $params['age'] == 19 )
-                        <option value="19">under 20</option>
-                        <option value=""> All </option>
-                        <option value="20">Between 20 ~ 29</option>
-                        <option value="30">Between 30 ~ 39</option>
-                        <option value="40">over 40</option>
-                      @elseif ( $params['age'] == 20 )
-                        <option value="20">Between 20 ~ 29</option>
-                        <option value=""> All </option>
-                        <option value="19">under 20</option>
-                        <option value="30">Between 30 ~ 39</option>
-                        <option value="40">over 40</option>
-                      @elseif ( $params['age'] == 30 )
-                        <option value="30">Between 30 ~ 39</option>
-                        <option value=""> All </option>
-                        <option value="19">under 20</option>
-                        <option value="20">Between 20 ~ 29</option>
-                        <option value="40">over 40</option>
-                      @elseif ( $params['age'] == 40 )
-                        <option value="40">over 40</option>
-                        <option value=""> All </option>
-                        <option value="19">under 20</option>
-                        <option value="20">Between 20 ~ 29</option>
-                        <option value="30">Between 30 ~ 39</option>
-                      @else
-                        <option value=""> All </option>
-                        <option value="19">under 20</option>
-                        <option value="20">Between 20 ~ 29</option>
-                        <option value="30">Between 30 ~ 39</option>
-                        <option value="40">over 40</option>
-                      @endif
-                  </select>
+                                    <select class="form-control mb-1" name="age">
+                                        @if ( $params['age'] == 19 )
+                                            <option value="19">under 20</option>
+                                            <option value=""> All </option>
+                                            <option value="20">Between 20 ~ 29</option>
+                                            <option value="30">Between 30 ~ 39</option>
+                                            <option value="40">over 40</option>
+                                        @elseif ( $params['age'] == 20 )
+                                            <option value="20">Between 20 ~ 29</option>
+                                            <option value=""> All </option>
+                                            <option value="19">under 20</option>
+                                            <option value="30">Between 30 ~ 39</option>
+                                            <option value="40">over 40</option>
+                                        @elseif ( $params['age'] == 30 )
+                                            <option value="30">Between 30 ~ 39</option>
+                                            <option value=""> All </option>
+                                            <option value="19">under 20</option>
+                                            <option value="20">Between 20 ~ 29</option>
+                                            <option value="40">over 40</option>
+                                        @elseif ( $params['age'] == 40 )
+                                            <option value="40">over 40</option>
+                                            <option value=""> All </option>
+                                            <option value="19">under 20</option>
+                                            <option value="20">Between 20 ~ 29</option>
+                                            <option value="30">Between 30 ~ 39</option>
+                                        @else
+                                            <option value=""> All </option>
+                                            <option value="19">under 20</option>
+                                            <option value="20">Between 20 ~ 29</option>
+                                            <option value="30">Between 30 ~ 39</option>
+                                            <option value="40">over 40</option>
+                                        @endif
+                                    </select>
 
-                  <div class="text-right">
-                    <button class="btn btn-primary mt-1" type="submit">Search</button>
-                  </div>
-                </form>
-              </div>
-            </div>
-
-          </div>
-        </div>
-
-        <div class="col-sm-7 pr-4">
-
-            <!-- フラッシュメッセージ -->
-            <div style="height:45px;">
-                @if (session('flash_success'))
-                    <div class="flash-js bg-success text-center text-white rounded py-2 my-0">
-                        {{ session('flash_success') }}
+                                    <div class="text-right">
+                                        <button class="btn btn-primary mt-1" type="submit">Search</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
                     </div>
-                @elseif (session('flash_alert'))
-                    <div class="flash-js bg-danger text-center text-white rounded py-2 my-0">
-                        {{ session('flash_alert') }}
+                </div>
+
+                <div class="col-sm-7 pr-4">
+                    <!-- フラッシュメッセージ -->
+                    <div style="height:45px;">
+                        @if (session('flash_success'))
+                            <div class="flash-js bg-success text-center text-white rounded py-2 my-0">
+                                {{ session('flash_success') }}
+                            </div>
+                        @elseif (session('flash_alert'))
+                            <div class="flash-js bg-danger text-center text-white rounded py-2 my-0">
+                                {{ session('flash_alert') }}
+                            </div>
+                        @endif
+                        <script type="text/javascript" src="/js/common/flash-message.js"></script>
                     </div>
-                @endif
-                <script type="text/javascript" src="/js/common/flash-message.js"></script>
-            </div>
 
-          <div class="favorite-contents-left">
-            <div class="text-white bg-dark favorite-head text-center h4 font-alegreya">Player Lists</div>
-            <table class="table m-0">
-              <thead class="thead-dark">
-                <th class="favorite-name-jp-w text-center">Name(jp)</th>
-                <th class="favorite-name-en-w text-center">Name(en)</th>
-                <th class="favorite-country-w text-center">country</th>
-                <th class="favorite-age-w text-center">age</th>
-                <th class="favorite-age-w text-center">add</th>
-              </thead>
-            </table>
-            <div class="favorite-tbody">
-              <table class="table table-striped">
-                <tbody>
-                @foreach ($player_lists as $player)
-                  <tr>
-                    <td class="favorite-td favorite-name-jp-w text-center pt-3"><a href="{{ $player['wiki_url'] }}">{{ $player['name_jp'] }}</a></td>
-                    <td class="favorite-td favorite-name-en-w text-center pt-3"><a href="{{ $player['wiki_url'] }}">{{ $player['name_en'] }}</a></td>
-                    <td class="favorite-td favorite-country-w text-center pt-3">{{ $player['country'] }}</td>
-                    <td class="favorite-td favorite-age-w text-center pt-3">{{ $player['age'] }}</td>
+                    <div class="favorite-contents-left">
+                        <div class="text-white bg-dark favorite-head text-center h4 font-alegreya">Player Lists</div>
+                        <table class="table m-0">
+                            <thead class="thead-dark">
+                                <th class="favorite-name-jp-w text-center">Name(jp)</th>
+                                <th class="favorite-name-en-w text-center">Name(en)</th>
+                                <th class="favorite-country-w text-center">country</th>
+                                <th class="favorite-age-w text-center">age</th>
+                                <th class="favorite-age-w text-center">add</th>
+                            </thead>
+                        </table>
+                        <div class="favorite-tbody">
+                            <table class="table table-striped">
+                                <tbody>
+                                    @foreach ($player_lists as $player)
+                                    <tr>
+                                        <td class="favorite-td favorite-name-jp-w text-center pt-3"><a href="{{ $player['wiki_url'] }}">{{ $player['name_jp'] }}</a></td>
+                                        <td class="favorite-td favorite-name-en-w text-center pt-3"><a href="{{ $player['wiki_url'] }}">{{ $player['name_en'] }}</a></td>
+                                        <td class="favorite-td favorite-country-w text-center pt-3">{{ $player['country'] }}</td>
+                                        <td class="favorite-td favorite-age-w text-center pt-3">{{ $player['age'] }}</td>
 
-                    <td class="favorite-td favorite-age-w text-center">
-                    @if ( $player['favorite_status'] == 0 )
-                      <form method="post" action="{{ route('favorite_player.add') }}">
-                        @csrf
-                        <input type="hidden" name="favorite_player_id" value="{{ $player['id'] }}">
-                        <button type="submit" class="btn btn-success p-1" style="width:66px;">add</button>
-                      </form>
-                    @else
-                      <form method="post" action="{{ route('favorite_player.remove') }}">
-                        @csrf
-                        <input type="hidden" name="favorite_player_id" value="{{ $player['id'] }}">
-                        <button type="submit" class="btn btn-danger p-1" style="width:66px;">remove</button>
-                      </form>
-                    @endif
-                    </td>
-                  </tr>
-                @endforeach
-                </tbody>
-              </table>
+                                        <td class="favorite-td favorite-age-w text-center">
+                                            @if ( $player['favorite_status'] == 0 )
+                                            <form method="post" action="{{ route('favorite_player.add') }}">
+                                                @csrf
+                                                <input type="hidden" name="favorite_player_id" value="{{ $player['id'] }}">
+                                                <button type="submit" class="btn btn-success p-1" style="width:66px;">add</button>
+                                            </form>
+                                            @else
+                                            <form method="post" action="{{ route('favorite_player.remove') }}">
+                                                @csrf
+                                                <input type="hidden" name="favorite_player_id" value="{{ $player['id'] }}">
+                                                <button type="submit" class="btn btn-danger p-1" style="width:66px;">remove</button>
+                                            </form>
+                                            @endif
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+
             </div>
-          </div>
         </div>
-
-
-      </div>
-    </div>
-    <script type="text/javascript" src="/js/common/modal.js"></script>
-  @else
-    @include('top.welcome')
-  @endif
+        <script type="text/javascript" src="/js/common/modal.js"></script>
+    @else
+        @include('top.welcome')
+    @endif
 @endsection
 
 
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,51 +1,51 @@
 <!doctype html>
 <html lang="ja">
-  <head>
-    <title>TennisApp</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <head>
+        <title>TennisApp</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    {{-- フォントのライブラリ --}}
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Alegreya+Sans+SC:300">
-    {{-- Fontawesome --}}
-    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+        {{-- フォントのライブラリ --}}
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Alegreya+Sans+SC:300">
+        {{-- Fontawesome --}}
+        <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
 
-    {{-- Vue.jsを使用してajax通信する時のため --}}
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    {{-- npm run devコマンドで生成した「app.css」と「app.js」を読み込んでいる --}}
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <script src="{{ asset('js/app.js')}}"></script>
+        {{-- Vue.jsを使用してajax通信する時のため --}}
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        {{-- npm run devコマンドで生成した「app.css」と「app.js」を読み込んでいる --}}
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <script src="{{ asset('js/app.js')}}"></script>
 
-    {{-- Bootstrap4.2で上書き TODO: ちゃんとライブラリをインストールする --}}
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+        {{-- Bootstrap4.2で上書き TODO: ちゃんとライブラリをインストールする --}}
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 
-    {{-- jQuery --}}
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+        {{-- jQuery --}}
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 
-    {{-- datepicker --}}
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/themes/base/jquery-ui.min.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-    <script type="text/javascript" src="/js/common/datepick.js"></script>
+        {{-- datepicker --}}
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/themes/base/jquery-ui.min.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+        <script type="text/javascript" src="/js/common/datepick.js"></script>
 
-    <link rel="stylesheet" href="/css/common/reset.css">
-    <link rel="stylesheet" href="/css/common/common.css">
-    <link rel="stylesheet" href="/css/common/nav_menu.css">
-    <link rel="stylesheet" href="/css/common/modal.css">
-    <link rel="stylesheet" href="/css/top/index.css">
-    <link rel="stylesheet" href="/css/favorite_player/index.css">
-    <script type="text/javascript" src="/js/top/scroll.js"></script>
-    <script type="text/javascript" src="/js/common/welcome.js"></script>
+        <link rel="stylesheet" href="/css/common/reset.css">
+        <link rel="stylesheet" href="/css/common/common.css">
+        <link rel="stylesheet" href="/css/common/nav_menu.css">
+        <link rel="stylesheet" href="/css/common/modal.css">
+        <link rel="stylesheet" href="/css/top/index.css">
+        <link rel="stylesheet" href="/css/favorite_player/index.css">
+        <script type="text/javascript" src="/js/top/scroll.js"></script>
+        <script type="text/javascript" src="/js/common/welcome.js"></script>
 
-  </head>
+    </head>
 
-  <body>
-    @include('shared.header')
+    <body>
+        @include('shared.header')
 
-    <main>
-      @yield('content')
-    </main>
+        <main>
+        @yield('content')
+        </main>
 
-    @include('shared.footer')
-    <script type="text/javascript" src="/js/common/nav_menu.js"></script>
-  </body>
+        @include('shared.footer')
+        <script type="text/javascript" src="/js/common/nav_menu.js"></script>
+    </body>
 </html>
