779f6f07f0d81d82b48954ff8c1496cbd8625123
0kkun <shinji.okumoto.01@gmail.com>
2020-11-13 00:22:40 +0900

    scrapeTourInfoコマンドのリファクタ

--- app/Console/Commands/scrapeTourInfo.php
+++ app/Console/Commands/scrapeTourInfo.php
@@ -11,7 +11,7 @@ use App\Repositories\Contracts\TourInformationsRepository;
 
 class scrapeTourInfo extends Command
 {
-    protected $signature = 'command:scrapeTourInfo {--sync : 同期処理}';
+    protected $signature = 'command:scrapeTourInfo';
     protected $description = '大会情報をスクレイピングで取得し保存する';
     private $tour_informations_repository;
 
@@ -37,30 +37,19 @@ class scrapeTourInfo extends Command
      */
     public function handle()
     {
-        $is_sync = $this->option('sync');
-
-        $this->info("【実行開始】");
+        $this->info("実行開始");
         $this->logger = new BatchLogger( 'scrapeTourInfo' );
 
         try {
-            $node_all = array();
+            $site_url = 'https://news.tennis365.net/news/tour/schedule/';
 
-            $goutte = GoutteFacade::request('GET', 'https://news.tennis365.net/news/tour/schedule/');
-            sleep(1);
+            $scrape_data = $this->execScrape( $site_url );
 
-            $goutte->filter('.scheBox td')->each(function ($node) use (&$node_all) {
-                if ( $node->count() !== 0 ) {
-                    array_push( $node_all, $node->text() );
-                } else {
-                    $this->info("スクレイピング実行できませんでした。");
-                }
-            });
-
-            $tour_data = $this->makeInsertValue( $node_all );
+            $tour_data = $this->makeInsertValue( $scrape_data );
 
             // バルクインサートで保存
             if ( !empty($tour_data) ) {
-                $this->tour_informations_repository->bulkInsertOrUpdate( $tour_data );
+                // $this->tour_informations_repository->bulkInsertOrUpdate( $tour_data );
             }
 
             $this->info("保存完了");
@@ -71,26 +60,50 @@ class scrapeTourInfo extends Command
             $this->logger->exception($e);
         }
         unset($this->logger);
-        $this->info("【実行終了】");
+        $this->info("実行終了");
+    }
+
+
+    /**
+     * スクレイピング実行
+     *
+     * @param string $site_url
+     * @return array
+     */
+    private function execScrape( string $site_url ): array
+    {
+        $data = array();
+
+        $goutte = GoutteFacade::request('GET', $site_url);
+        sleep(1);
+
+        $goutte->filter('.scheBox td')->each(function ($node) use (&$data) {
+            if ( $node->count() !== 0 ) {
+                array_push( $data, $node->text() );
+            } else {
+                $this->info("スクレイピング実行できませんでした。");
+            }
+        });
+        return $data;
     }
 
 
     /**
      * バルクインサートで保存用に加工する
      *
-     * @param array $node_all
+     * @param array $data
      * @return array
      */
-    private function makeInsertValue( array $node_all ):array
+    private function makeInsertValue( array $data ):array
     {
         $year = Carbon::now()->year;
         $today = Carbon::now();
 
         // 保存したいデータは6番目から始まる
-        for ($i=6; $i<count($node_all); $i=$i+6) {
+        for ($i=6; $i<count($data); $i=$i+6) {
 
             // yyyy/m/dになるように加工
-            $divided_date = explode('〜', $node_all[$i]);
+            $divided_date = explode('〜', $data[$i]);
             $start_date = $year . '/' . $divided_date[0];
             $end_date = $year . '/' . $divided_date[1];
 
@@ -98,11 +111,11 @@ class scrapeTourInfo extends Command
                 'start_date' => $start_date ?? null,
                 'end_date'   => $end_date ?? null,
                 'year'       => $year,
-                'name'       => $node_all[$i+1] ?? null,
-                'category'   => $node_all[$i+2] ?? null,
-                'location'   => $node_all[$i+3] ?? null,
-                'surface'    => $node_all[$i+4] ?? null,
-                'draw_num'   => (string) $node_all[$i+5] ?? null,
+                'name'       => $data[$i+1] ?? null,
+                'category'   => $data[$i+2] ?? null,
+                'location'   => $data[$i+3] ?? null,
+                'surface'    => $data[$i+4] ?? null,
+                'draw_num'   => (string) $data[$i+5] ?? null,
                 'created_at' => $today,
                 'updated_at' => $today,
             ];
