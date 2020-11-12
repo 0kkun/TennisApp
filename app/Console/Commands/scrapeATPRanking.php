c6547108acaf6bf1a9a0c79848900a36642c6b3b
0kkun <shinji.okumoto.01@gmail.com>
2020-11-12 23:16:31 +0900

    scrapeATPRankingコマンドのリファクタ

--- app/Console/Commands/scrapeATPRanking.php
+++ app/Console/Commands/scrapeATPRanking.php
@@ -12,7 +12,7 @@ use App\Repositories\Contracts\AtpRankingsRepository;
 class scrapeATPRanking extends Command
 {
 
-    protected $signature = 'command:scrapeATPRanking {--sync : 同期処理}';
+    protected $signature = 'command:scrapeATPRanking';
     protected $description = 'ATPランキングをスクレイピングで取得し保存するコマンド';
     private $atp_rankings_repository;
 
@@ -32,68 +32,33 @@ class scrapeATPRanking extends Command
 
 
     /**
-     * ランキング情報を取得する。
+     * ATPランキング情報を取得する。
      * 1~50位と50~100位が2ページに分かれているのでスクレイピングは2回実行している
      *
      * @return mixed
      */
     public function handle()
     {
-        /**
-         * MEMO:
-         * ->getUri()       最後にたどり着いたURL
-         * ->text()         テキストのみ取得
-         * ->first()        一番最初のを取得
-         * ->html()         HTMLを取得
-         * ->attr('href')   属性を取得
-         * ->eq(0)          同一タグがある場合、登場順番を指定できる
-         */
-
-        $is_sync = $this->option('sync');
-
         $this->info("実行開始");
         $this->logger = new BatchLogger( 'scrapeATPRanking' );
 
         try {
-            $rank = array();
-            $name = array();
-            $country = array();
-            $point = array();
-
-            $goutte_part1 = GoutteFacade::request('GET', 'https://sportsnavi.ht.kyodo-d.jp/tennis/ranking/atp/point/');
+            $goutte_first = GoutteFacade::request('GET', 'https://sportsnavi.ht.kyodo-d.jp/tennis/ranking/atp/point/');
             sleep(1);
-            $goutte_part2 = GoutteFacade::request('GET', 'https://sportsnavi.ht.kyodo-d.jp/tennis/ranking/atp/point/?p=2');
+            $goutte_second = GoutteFacade::request('GET', 'https://sportsnavi.ht.kyodo-d.jp/tennis/ranking/atp/point/?p=2');
             sleep(1);
 
-            // 1 ~ 50位のランキングを取得
-            $goutte_part1->filter('tbody tr')->each(function ($node) use (&$rank, &$name, &$country, &$point){
-                if ( $node->count() > 0 ) {
-                    array_push( $rank, $node->filter('td')->eq(0)->text() );
-                    array_push( $name, $node->filter('td')->eq(1)->text() );
-                    array_push( $country, $node->filter('td')->eq(2)->text() );
-                    array_push( $point, $node->filter('td')->eq(3)->text() );
-                } else {
-                    $this->info("スクレイピング実行できませんでした。");
-                }
-            });
+            $scraped_data_first = $this->execScrape( $goutte_first );
 
-            // 50 ~ 100位のランキングを取得
-            $goutte_part2->filter('tbody tr')->each(function ($node) use (&$rank, &$name, &$country, &$point){
-                if ( $node->count() > 0 ) {
-                    array_push( $rank, $node->filter('td')->eq(0)->text() );
-                    array_push( $name, $node->filter('td')->eq(1)->text() );
-                    array_push( $country, $node->filter('td')->eq(2)->text() );
-                    array_push( $point, $node->filter('td')->eq(3)->text() );
-                } else {
-                    $this->info("スクレイピング実行できませんでした。");
-                }
-            });
+            $scraped_data_second = $this->execScrape( $goutte_second );
 
-            $ranking_data = $this->makeInsertValue( $rank, $name, $country, $point );
+            $scraped_data = $this->integrateArrayInArray( $scraped_data_first, $scraped_data_second );
+
+            $atp_ranking_data = $this->makeInsertValue( $scraped_data );
 
             // バルクインサートで保存
-            if ( !empty($ranking_data) ) {
-                $this->atp_rankings_repository->bulkInsertOrUpdate($ranking_data);
+            if ( !empty($atp_ranking_data) ) {
+                $this->atp_rankings_repository->bulkInsertOrUpdate($atp_ranking_data);
             }
 
             $this->info("保存完了");
@@ -110,6 +75,59 @@ class scrapeATPRanking extends Command
 
 
     /**
+     * スクレイピング実行メソッド
+     *
+     * @param mixed $goutte
+     * @return array
+     */
+    private function execScrape( $goutte ):array
+    {
+        // 各キーに配列を設定
+        $data['rank']    = array();
+        $data['name']    = array();
+        $data['country'] = array();
+        $data['point']   = array();
+
+        $goutte->filter('tbody tr')->each(function ($node) use (&$data){
+            if ( $node->count() > 0 ) {
+                array_push( $data['rank'], $node->filter('td')->eq(0)->text() );
+                array_push( $data['name'], $node->filter('td')->eq(1)->text() );
+                array_push( $data['country'], $node->filter('td')->eq(2)->text() );
+                array_push( $data['point'], $node->filter('td')->eq(3)->text() );
+            } else {
+                $this->info("スクレイピング実行できませんでした。");
+            }
+        });
+        return $data;
+    }
+
+
+    /**
+     * 配列の中の配列を結合する。
+     *
+     * @param array $first_array
+     * @param array $second_array
+     * @return void
+     */
+    private function integrateArrayInArray( array $first_array, array $second_array): array
+    {
+        $integrated_array = array();
+
+        if ( !empty($first_array) && !empty($second_array) ) {
+
+            $integrated_array['rank'] = array_merge($first_array['rank'], $second_array['rank']);
+            $integrated_array['name'] = array_merge($first_array['name'], $second_array['name']);
+            $integrated_array['country'] = array_merge($first_array['country'], $second_array['country']);
+            $integrated_array['point'] = array_merge($first_array['point'], $second_array['point']);
+
+            return $integrated_array;
+
+        } else {
+            return $integrated_array;
+        }
+    }
+
+    /**
      * レコード保存用のデータを作成。
      *
      * @param array $rank
@@ -118,26 +136,27 @@ class scrapeATPRanking extends Command
      * @param array $point
      * @return array
      */
-    private function makeInsertValue( array $rank, array $name, array $country, array $point ): array
+    private function makeInsertValue( array $scraped_data ): array
     {
-        $count = count( $rank );
+        $count = count( $scraped_data['rank'] );
+
         $today = Carbon::now();
         $ymd   = $today->copy()->format('Y-m-d');
 
-        $point = $this->changeToNumeric( $point );
+        $scraped_data['point'] = $this->changeToNumeric( $scraped_data['point'] );
 
         // 前回のランキングを一括取得
         $pre_rankings_all = $this->atp_rankings_repository->getAll();
 
         for ( $i=0; $i<$count; $i++ ) {
             // 選手の前回のランキングを取得
-            $pre_ranking = $pre_rankings_all->where('name', $name[$i])->first();
+            $pre_ranking = $pre_rankings_all->where('name', $scraped_data['name'][$i])->first();
 
             $value[$i] = [
-                'rank'       => (int) $rank[$i],
-                'name'       => $name[$i],
-                'country'    => $country[$i],
-                'point'      => $point[$i],
+                'rank'       => $scraped_data['rank'][$i],
+                'name'       => $scraped_data['name'][$i],
+                'country'    => $scraped_data['country'][$i],
+                'point'      => $scraped_data['point'][$i],
                 'ymd'        => $ymd,
                 'created_at' => $today,
                 'updated_at' => $today,
