5f9a5cd15142a5d0acffa62e0de1b9860d16cfca
0kkun <shinji.okumoto.01@gmail.com>
2020-11-15 05:21:48 +0900

    ScrapeTennisNewsJobコマンドのリファクタ

--- app/Jobs/ScrapeTennisNewsJob.php
+++ app/Jobs/ScrapeTennisNewsJob.php
@@ -30,7 +30,7 @@ class ScrapeTennisNewsJob implements ShouldQueue
      * TODO: 海外の記事も取得し、翻訳した物を保存できるようにしたい
      * FIXME: 2回連続で実行時にupdateする際、updated_atのtimezoneがアジアではなくなるバグ有り
      * 
-     * 実行スコア
+     * 実行ログ
      * [2020-10-19 03:43:44] local.INFO: [ScrapeTennisNewsJob] 保存完了 : 5.5461130142212秒  
      * [2020-10-19 03:43:44] local.INFO: [ END ] ScrapeTennisNewsJob 処理時間: 5.5464880466461秒
      *
@@ -45,14 +45,17 @@ class ScrapeTennisNewsJob implements ShouldQueue
         $this->logger = new BatchLogger('ScrapeTennisNewsJob');
 
         try {
-            $title     = array();
-            $url       = array();
-            $post_time = array();
+            $scrape_data = [
+                'title'     => [],
+                'url'       => [],
+                'post_time' => [],
+            ];
 
             $yahoo_url_page1 = 'https://sports.yahoo.co.jp/news/list?id=tennis';
             $yahoo_url_page2 = 'https://sports.yahoo.co.jp/news/list?id=tennis&fmi=tennis&p=2';
             $yahoo_url_page3 = 'https://sports.yahoo.co.jp/news/list?id=tennis&fmi=tennis&p=3';
 
+            // スクレイピング時のhtmlクラスを格納
             $pattern = [
                 'all'       => '.textNews',
                 'title'     => '.articleTitle',
@@ -61,9 +64,9 @@ class ScrapeTennisNewsJob implements ShouldQueue
             ];
 
             // スクレイピング実行
-            $this->scrapeTennisNewsSite( $yahoo_url_page1, $pattern, $title, $url, $post_time );
-            $this->scrapeTennisNewsSite( $yahoo_url_page2, $pattern, $title, $url, $post_time );
-            $article_data = $this->scrapeTennisNewsSite( $yahoo_url_page3, $pattern, $title, $url, $post_time );
+            $this->scrapeTennisNewsSite( $yahoo_url_page1, $pattern, $scrape_data );
+            $this->scrapeTennisNewsSite( $yahoo_url_page2, $pattern, $scrape_data );
+            $article_data = $this->scrapeTennisNewsSite( $yahoo_url_page3, $pattern, $scrape_data );
 
             // バルクインサートで保存
             if ( !empty($article_data) ) {
@@ -81,47 +84,55 @@ class ScrapeTennisNewsJob implements ShouldQueue
 
 
 
-    private function scrapeTennisNewsSite( string $site_url, array $pattern, array &$title, array &$url, array &$post_time ): array
+    /**
+     * テニスニュースサイトのスクレイピング実行
+     *
+     * @param string $site_url
+     * @param array $pattern
+     * @param array $data
+     * @return array
+     */
+    private function scrapeTennisNewsSite( string $site_url, array $pattern, array &$data ): array
     {
-      $goutte = GoutteFacade::request('GET', $site_url);
-      sleep(1);
+        $goutte = GoutteFacade::request('GET', $site_url);
+        sleep(1);
 
-      $goutte->filter($pattern['all'])->each(function ($node) use (&$pattern, &$title, &$url, &$post_time) {
-        if ( $node->count() > 0 ) {
-            array_push( $title, $node->filter($pattern['title'])->text() );
-            array_push( $url, $node->filter($pattern['url'])->attr('href') );
-            array_push( $post_time, $node->filter($pattern['post_time'])->text() );
-        } else {
-            Log::info('スクレイピング実行できませんでした');
-        }
-      });
+        $goutte->filter($pattern['all'])->each(function ($node) use (&$pattern, &$data) {
+            if ( $node->count() > 0 ) {
+                array_push( $data['title'], $node->filter($pattern['title'])->text() );
+                array_push( $data['url'], $node->filter($pattern['url'])->attr('href') );
+                array_push( $data['post_time'], $node->filter($pattern['post_time'])->text() );
+            } else {
+                Log::info('スクレイピング実行できませんでした');
+            }
+        });
 
-      $tennis_news_data = $this->makeInsertValue( $title, $url, $post_time);
+        $tennis_news_data = $this->makeInsertValue( $data );
 
-      return $tennis_news_data;
+        return $tennis_news_data;
     }
 
 
     /**
      * レコード保存用のデータを作成。
      *
-     * @param array $title
+     * @param array $data['title']
      * @param array $news_url
      * @param array $post_time
      * @return array
      */
-    private function makeInsertValue( array $title, array $url, array $post_time ): array
+    private function makeInsertValue( array $data ): array
     {
-        $count = count( $title );
+        $count = count( $data['title'] );
         $today = Carbon::now();
 
-        $post_time = $this->formatToCarbon( $post_time );
+        $data['post_time'] = $this->formatToCarbon( $data['post_time'] );
 
         for ( $i=0; $i<$count; $i++ ) {
             $value[$i] = [
-                'title'      => $title[$i],
-                'url'        => $url[$i],
-                'post_time'  => $post_time[$i],
+                'title'      => $data['title'][$i],
+                'url'        => $data['url'][$i],
+                'post_time'  => $data['post_time'][$i],
                 'created_at' => $today,
                 'updated_at' => $today
             ];
