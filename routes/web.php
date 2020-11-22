580dae89e77eaa365ef2fe23e5b4089905224496
0kkun <shinji.okumoto.01@gmail.com>
2020-11-22 15:02:36 +0900

    ページアクセスのテスト実装

--- routes/web.php
+++ routes/web.php
@@ -16,15 +16,15 @@
 Route::get('/', 'TopController@index')->name('top.index');
 
 Route::prefix('favorite_player')->group(function () {
-  Route::get('/', 'FavoritePlayerController@index')->name('favorite_player.index');
-  Route::post('/add', 'FavoritePlayerController@add')->name('favorite_player.add');
-  Route::post('/remove', 'FavoritePlayerController@remove')->name('favorite_player.remove');
+    Route::get('/', 'FavoritePlayerController@index')->name('favorite_player.index');
+    Route::post('/add', 'FavoritePlayerController@add')->name('favorite_player.add');
+    Route::post('/remove', 'FavoritePlayerController@remove')->name('favorite_player.remove');
 });
 
 Route::prefix('favorite_brand')->group(function () {
-  Route::get('/', 'FavoriteBrandController@index')->name('favorite_brand.index');
-  Route::post('/add', 'FavoriteBrandController@add')->name('favorite_brand.add');
-  Route::post('/remove', 'FavoriteBrandController@remove')->name('favorite_brand.remove');
+    Route::get('/', 'FavoriteBrandController@index')->name('favorite_brand.index');
+    Route::post('/add', 'FavoriteBrandController@add')->name('favorite_brand.add');
+    Route::post('/remove', 'FavoriteBrandController@remove')->name('favorite_brand.remove');
 });
 
 Route::get('/analysis', 'AnalysisController@index')->name('analysis.index');
