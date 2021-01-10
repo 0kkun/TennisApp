1749602b8c2096717a18437d4ab107afbd4ec654
0kkun <shinji.okumoto.01@gmail.com>
2021-01-10 14:38:35 +0900

    cssをsassをコンパイルしたものを使用するよう変更(mixinの利用)

--- server/resources/views/layouts/layout.blade.php
+++ server/resources/views/layouts/layout.blade.php
@@ -4,17 +4,17 @@
         <title>TennisApp</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        {{-- Vue.jsを使用してajax通信する時のため --}}
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        {{-- npm run devコマンドで生成した「app.css」と「app.js」を読み込んでいる --}}
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <script src="{{ asset('js/app.js')}}"></script>
 
         {{-- フォントのライブラリ --}}
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Alegreya+Sans+SC:300">
         {{-- Fontawesome --}}
         <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
-
-        {{-- Vue.jsを使用してajax通信する時のため --}}
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        {{-- npm run devコマンドで生成した「app.css」と「app.js」を読み込んでいる --}}
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        <script src="{{ asset('js/app.js')}}"></script>
 
         {{-- Bootstrap4.2で上書き TODO: ちゃんとライブラリをインストールする --}}
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
@@ -22,13 +22,6 @@
         {{-- datepicker --}}
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/themes/base/jquery-ui.min.css">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-
-        <link rel="stylesheet" href="/css/common/reset.css">
-        <link rel="stylesheet" href="/css/common/common.css">
-        <link rel="stylesheet" href="/css/common/nav_menu.css">
-        <link rel="stylesheet" href="/css/common/modal.css">
-        <link rel="stylesheet" href="/css/top/index.css">
-        <link rel="stylesheet" href="/css/favorite_player/index.css">
     </head>
 
     <body>
