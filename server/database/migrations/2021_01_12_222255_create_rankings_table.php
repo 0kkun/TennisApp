b5d83effc1b24376c40940ca27374c5010d0992e
0kkun <shinji.okumoto.01@gmail.com>
2021-01-12 23:16:59 +0900

    マイグレーションファイル修正

--- server/database/migrations/2021_01_12_222255_create_rankings_table.php
+++ server/database/migrations/2021_01_12_222255_create_rankings_table.php
@@ -16,15 +16,15 @@ class CreateRankingsTable extends Migration
         Schema::create('rankings', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('rank')->length(4)->comment('最新のランキング');
-            $table->integer('most_heighest')->length(4)->nullable()->comment('これまでの一番高いランキング');
+            $table->integer('most_highest')->length(4)->nullable()->comment('これまでの一番高いランキング');
             $table->string('name', 50)->commnet('選手名');
             $table->integer('age')->length(3)->comment('年齢');
             $table->string('country', 50)->comment('所属国');
             $table->integer('point')->length(6)->commnet('ATPポイント');
             $table->integer('rank_change')->length(6)->commnet('前回ランクからの変化');
             $table->integer('point_change')->length(6)->comment('前回ポイントからの変化');
-            $table->string('current_tour_result', 50)->commnet('現在参加している大会の結果');
-            $table->string('pre_tour_result', 50)->comment('前回大会の結果');
+            $table->string('current_tour_result', 100)->commnet('現在参加している大会の結果');
+            $table->string('pre_tour_result', 100)->comment('前回大会の結果');
             $table->integer('next_point')->length(6)->comment('次に勝つと入手できるポイント');
             $table->integer('max_point')->length(6)->commnet('現在参加している大会で全部勝てばどれだけポイントが入手できるか');
             $table->timestamps();
