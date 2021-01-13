1eeafbddac38458d856465818eeb565acad8772e
0kkun <shinji.okumoto.01@gmail.com>
2021-01-13 23:15:51 +0900

    Controllerにranking取得API実装

--- server/app/Http/Controllers/RankingController.php
+++ server/app/Http/Controllers/RankingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Repositories\Contracts\RankingRepository;
+use Exception;
 
 class RankingController extends Controller
 {
@@ -24,5 +25,23 @@ class RankingController extends Controller
     public function index()
     {
         return view('ranking.index');
+    }
+
+    /**
+     * ランキング取得用のAPI
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function fetchRankings(Request $request)
+    {
+        try {
+            $num = $request->input('num');
+            $response = $this->ranking_repository->fetchRankings($num);
+            return request()->json(200, $response);
+
+        } catch (Exception $e) {
+            return response()->json($e->getMessage(), 500);
+        }
     }
 }
