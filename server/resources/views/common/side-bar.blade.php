ecadbbd041edda6488b3716f70c9a13658d61870
0kkun <shinji.okumoto.01@gmail.com>
2021-01-13 23:54:21 +0900

    サイドバーにブランド用リンク追加

--- server/resources/views/common/side-bar.blade.php
+++ server/resources/views/common/side-bar.blade.php
@@ -18,9 +18,9 @@
         </a>
     </div>
     <div class="mb-1">
-        <a class="pl-2 pt-2 pb-2 h2 btn-block rounded-pill d-flex" href="{{ route('ranking.index') }}">
+        <a class="pl-2 pt-2 pb-2 h2 btn-block rounded-pill d-flex" href="{{ route('favorite_brand.top') }}">
             <div class="fas fa-star pl-1 pr-1"></div>
-            <div class="pl-3 d-none d-md-block">Favorite</div>
+            <div class="pl-3 d-none d-md-block">Favorite Brand</div>
         </a>
     </div>
 </div>
