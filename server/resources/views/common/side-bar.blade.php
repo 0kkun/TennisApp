749c84af1cc0b69c6d147b72d52ccf81237376b6
0kkun <shinji.okumoto.01@gmail.com>
2021-01-14 09:09:59 +0900

    side barにnewsトップページへのリンク追加

--- server/resources/views/common/side-bar.blade.php
+++ server/resources/views/common/side-bar.blade.php
@@ -6,7 +6,7 @@
         </a>
     </div>
     <div class="mb-1">
-        <a class="pl-2 pt-2 pb-2 h2 btn-block rounded-pill d-flex" href="{{ route('ranking.index') }}">
+        <a class="pl-2 pt-2 pb-2 h2 btn-block rounded-pill d-flex" href="{{ route('news.index') }}">
             <div class="fas fa-newspaper pl-1 pr-1"></div>
             <div class="pl-3 d-none d-md-block">News</div>
         </a>
