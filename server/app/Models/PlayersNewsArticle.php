b6497e8dd8474f251249c22ed9fed85fb81d4df6
0kkun <shinji.okumoto.01@gmail.com>
2021-01-15 08:42:34 +0900

    PlayersNewsArticleモデル作成

---
+++ server/app/Models/PlayersNewsArticle.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PlayersNewsArticle extends Model
+{
+    use BulkInsertOrUpdateTrait;
+
+    protected $table = 'players_news_articles';
+    protected $guarded = array('id');
+
+    public $timestamps = true;
+
+    /**
+     * バルクインサート時のカラム指定
+     *
+     * @return array
+     */
+    protected function getUpdateColumnsOnDuplicate(): array
+    {
+        return [
+            'title',
+            'image',
+            'url',
+            'post_time',
+            'vender',
+            'updated_at',
+        ];
+    }
+}
