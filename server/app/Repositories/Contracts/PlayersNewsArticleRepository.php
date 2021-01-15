16b6f72ed3015de0b97746ce8498ab6508a0230f
0kkun <shinji.okumoto.01@gmail.com>
2021-01-15 11:35:50 +0900

    nameからタイトルを検索しニュースを取得するリポジトリメソッド実装

--- server/app/Repositories/Contracts/PlayersNewsArticleRepository.php
+++ server/app/Repositories/Contracts/PlayersNewsArticleRepository.php
@@ -14,4 +14,23 @@ interface PlayersNewsArticleRepository
      * @return void
      */
     public function bulkInsertOrUpdate($data): void;
+
+    /**
+     * 指定の数、記事を取得する
+     *
+     * @param integer $num
+     * @param bool $is_paginate
+     * @return Collection|LengthAwarePaginator
+     */
+    public function fetchArticles(int $num, bool $is_paginate);
+
+    /**
+     * like句で選手名で検索し記事を取得する
+     *
+     * @param array $player_names
+     * @param integer $num
+     * @param boolean $is_paginate
+     * @return Collection|LengthAwarePaginator
+     */
+    public function fetchArticlesByPlayerNames(array $player_names, int $num, bool $is_paginate);
 }
--- server/app/Repositories/Eloquents/EloquentPlayersNewsArticleRepository.php
+++ server/app/Repositories/Eloquents/EloquentPlayersNewsArticleRepository.php
@@ -31,4 +31,51 @@ class EloquentPlayersNewsArticleRepository implements PlayersNewsArticleReposito
     {
         $this->players_news_articles->bulkInsertOrUpdate($data);
     }
+
+
+    /**
+     * 指定の数、記事を取得する
+     *
+     * @param integer $num
+     * @param bool $is_paginate
+     * @return Collection|LengthAwarePaginator
+     */
+    public function fetchArticles(int $num, bool $is_paginate)
+    {
+        return $this->players_news_articles
+                    ->limit($num)
+                    ->orderBy('post_time', 'desc')
+                    ->when($is_paginate, function ($query) {
+                        // パラメータ名を指定することでページネーションを独立させる
+                        return $query->paginate(config('const.PAGINATE.NEWS_LINK_NUM'), ["*"], 'newspage'); 
+                    }, function ($query) {
+                        return $query->get();
+                    });
+    }
+
+    /**
+     * like句で選手名で検索し記事を取得する
+     *
+     * @param array $player_names
+     * @param integer $num
+     * @param boolean $is_paginate
+     * @return Collection|LengthAwarePaginator
+     */
+    public function fetchArticlesByPlayerNames(array $player_names, int $num, bool $is_paginate)
+    {
+        return $this->players_news_articles
+                    ->where( function ($query) use ($player_names) {
+                        for ($i=0; $i<count($player_names); $i++) {
+                            $query->orWhere('title', 'like', '%' . $player_names[$i] . '%');
+                        }
+                    })
+                    ->limit($num)
+                    ->orderBy('post_time', 'desc')
+                    ->when($is_paginate, function ($query) {
+                        // パラメータ名を指定することでページネーションを独立させる
+                        return $query->paginate(config('const.PAGINATE.NEWS_LINK_NUM'), ["*"], 'newspage'); 
+                    }, function ($query) {
+                        return $query->get();
+                    });
+    }
 }
