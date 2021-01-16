4d98d71ed7096777a9d4711227aa2f62e50bdcf6
0kkun <shinji.okumoto.01@gmail.com>
2021-01-16 11:30:48 +0900

    ツアー情報取得・保存用のリポジトリメソッド実装

--- server/app/Repositories/Contracts/TourScheduleRepository.php
+++ server/app/Repositories/Contracts/TourScheduleRepository.php
@@ -2,6 +2,27 @@
 
 namespace App\Repositories\Contracts;
 
+use Illuminate\Support\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
+
 interface TourScheduleRepository
 {
+    /**
+     * 開催中の大会とこれから始まる予定の大会だけを取得する
+     * すでに終了した大会は取得しない
+     *
+     * @param $is_paginate
+     * @return Collection|LengthAwarePaginator
+     */  
+    public function getAll(bool $is_paginate);
+
+
+    /**
+     * バルクインサート
+     * テーブルに保存・更新する
+     * 
+     * @param array|Model|Collection $data
+     * @return void
+     */
+    public function bulkInsertOrUpdate($data): void;
 }
--- server/app/Repositories/Eloquents/EloquentTourScheduleRepository.php
+++ server/app/Repositories/Eloquents/EloquentTourScheduleRepository.php
@@ -3,7 +3,52 @@
 namespace App\Repositories\Eloquents;
 
 use App\Repositories\Contracts\TourScheduleRepository;
+use App\Models\TourSchedule;
+use Carbon\Carbon;
 
 class EloquentTourScheduleRepository implements TourScheduleRepository
 {
+    protected $tour_schedules;
+
+    /**
+    * @param object $tour_schedules
+    */
+    public function __construct(
+        TourSchedule $tour_schedules
+    )
+    {
+        $this->tour_schedules = $tour_schedules;
+    }
+
+    /**
+     * 開催中の大会とこれから始まる予定の大会だけを取得する
+     * すでに終了した大会は取得しない
+     *
+     * @param bool $is_paginate
+     * @return Collection
+     */
+    public function getAll(bool $is_paginate)
+    {
+        $today = Carbon::today();
+
+        return $this->tour_schedules
+                    ->where('end_date', '>=', $today)
+                    ->orderBy('start_date', 'asc')
+                    ->when($is_paginate, function ($query) {
+                        return $query->paginate(config('const.PAGINATE.NEWS_LINK_NUM'), ["*"], 'tour'); 
+                    }, function ($query) {
+                        return $query->get();
+                    });
+    }
+
+    /**
+     * バルクインサート処理
+     *
+     * @param  Collection|Model|array $data
+     * @return void
+     */
+    public function bulkInsertOrUpdate($data): void
+    {
+        $this->tour_schedules->bulkInsertOrUpdate($data);
+    }
 }
