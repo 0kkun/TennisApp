3f999d2d7a4e482c8b7cd47e6f71c6f9aefe0465
0kkun <shinji.okumoto.01@gmail.com>
2021-01-17 01:28:18 +0900

    選手名の日本語訳と大会結果の日本語訳も同時に保存できるようにテーブルを変更

--- server/database/migrations/2021_01_12_222255_create_rankings_table.php
+++ server/database/migrations/2021_01_12_222255_create_rankings_table.php
@@ -17,17 +17,23 @@ class CreateRankingsTable extends Migration
             $table->increments('id');
             $table->integer('rank')->length(4)->comment('最新のランキング');
             $table->integer('most_highest')->length(4)->nullable()->comment('これまでの一番高いランキング');
-            $table->string('name', 50)->commnet('選手名');
+            $table->string('name_en', 100)->commnet('選手名');
+            $table->string('name_jp', 100)->commnet('選手名');
             $table->integer('age')->length(3)->comment('年齢');
-            $table->string('country', 50)->comment('所属国');
+            $table->string('country', 100)->comment('所属国');
             $table->integer('point')->length(6)->commnet('ATPポイント');
             $table->integer('rank_change')->length(6)->commnet('前回ランクからの変化');
             $table->integer('point_change')->length(6)->comment('前回ポイントからの変化');
-            $table->string('current_tour_result', 100)->commnet('現在参加している大会の結果');
-            $table->string('pre_tour_result', 100)->comment('前回大会の結果');
+            $table->string('current_tour_result_en', 200)->commnet('現在参加している大会の結果');
+            $table->string('current_tour_result_jp', 200)->commnet('現在参加している大会の結果');
+            $table->string('pre_tour_result_en', 200)->comment('前回大会の結果');
+            $table->string('pre_tour_result_jp', 200)->comment('前回大会の結果');
             $table->integer('next_point')->length(6)->comment('次に勝つと入手できるポイント');
             $table->integer('max_point')->length(6)->commnet('現在参加している大会で全部勝てばどれだけポイントが入手できるか');
+            $table->date('ymd')->comment('ランキングが更新された日');
             $table->timestamps();
+
+            $table->unique(['name_en','ymd']);
         });
     }
 
