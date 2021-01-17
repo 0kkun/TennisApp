4fea25d30e9a1639df9a6d715423c24f2a108ce1
0kkun <shinji.okumoto.01@gmail.com>
2021-01-17 22:26:45 +0900

    サイドバーのアイコン変更

--- server/resources/views/common/side-bar.blade.php
+++ server/resources/views/common/side-bar.blade.php
@@ -1,7 +1,7 @@
 <div class="side-bar font-alegreya font-weight-bold">
     <div class="mt-3 mb-1">
         <a class="pl-1 pt-2 pb-2 h2 btn-block rounded-pill d-flex" style="text-decoration:none;" href="{{ route('home.index') }}">
-            <div class="fas fa-home pl-1"></div>
+            <div class="fas fa-home pl-1 pr-1"></div>
             <div class="pl-3 d-none d-md-block">Home</div>
         </a>
     </div>
@@ -25,7 +25,7 @@
     </div>
     <div class="mb-1">
         <a class="pl-1 pt-2 pb-2 h2 btn-block rounded-pill d-flex" style="text-decoration:none;" href="{{ route('favorite_player.top') }}">
-            <div class="fas fa-star pr-1"></div>
+            <div class="fas fa-address-book pl-1 pr-1"></div>
             <div class="pl-3 d-none d-md-block">Player</div>
         </a>
     </div>
