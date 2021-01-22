2a31f31b7d9e25deff4a0712ecc5dd1b395fee2b
0kkun <shinji.okumoto.01@gmail.com>
2021-01-22 23:26:41 +0900

    ランキングのグラフ用Vueコンポーネント作成

---
+++ server/resources/js/components/RankingChartComponent.vue
@@ -0,0 +1,37 @@
+<script>
+// ここでチャートの種類を選択
+/**
+ * Bar       : 棒グラフ
+ * Line      : 折れ線グラフ
+ * Doughnut  : ドーナツグラフ
+ * Pie       : 円グラフ
+ * Radar     : レーダーグラフ
+ * Polararea : 極域グラフ
+ * Bubble    : バブルグラフ
+ * Scatter   : 散布図（mixinでは未対応とのこと）
+ **/
+import { Line } from 'vue-chartjs';
+
+export default {
+    extends: Line,
+    name: 'LineChart',
+    data:() => ({
+    chartdata: {
+        labels: ["12:00", "15:00", "18:00", "21:00", "0:00", "3:00", "6:00", "9:00"],
+        datasets: [
+            {
+                label: 'temperature',
+                data: [23, 25, 22, 20, 19, 19, 19, 20, 23]
+            }
+        ]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false
+    }
+    }),
+    mounted () {
+        this.renderChart(this.chartdata, this.options);
+    },
+}
+</script>
