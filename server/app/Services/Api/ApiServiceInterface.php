9e4c5685fc85a7bc012ab78b2450c204e6f510cf
0kkun <shinji.okumoto.01@gmail.com>
2021-01-23 18:22:38 +0900

    APIのコントローラ間で共通のメソッドをサービスに移管

--- server/app/Services/Api/ApiServiceInterface.php
+++ server/app/Services/Api/ApiServiceInterface.php
@@ -2,8 +2,25 @@
 
 namespace App\Services\Api;
 
-use Illuminate\Support\Collection;
+use Illuminate\Http\Request;
+use Exception;
 
 interface ApiServiceInterface
 {
+    /**
+     * APIリクエストのパラメータチェック
+     *
+     * @param Request $request
+     * @param array $keys
+     * @return integer
+     */
+    public function checkArgs(Request $request, array $keys): int;
+
+    /**
+     * レスポンス用のエラー情報をまとめる
+     *
+     * @param Exception $e
+     * @return array
+     */
+    public function makeErrorInfo(Exception $e): array;
 }
