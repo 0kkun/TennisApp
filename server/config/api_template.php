ff67d52096f227977324a4922404d8d64abce80f
0kkun <shinji.okumoto.01@gmail.com>
2021-01-23 18:29:08 +0900

    configにdeleteのステータスを追加

--- server/config/api_template.php
+++ server/config/api_template.php
@@ -14,6 +14,7 @@ return [
     'result_status' => [
         'success'      => 200,
         'created'      => 201,
+        'deleted'      => 202,
         'no_content'   => 204,
         'bad_request'  => 400,
         'server_error' => 500
