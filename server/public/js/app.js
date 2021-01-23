08687fc9b5240fcd48c0de2046189642faf077b5
0kkun <shinji.okumoto.01@gmail.com>
2021-01-23 11:18:57 +0900

    RankingChartComponentのリファクタ

--- server/public/js/app.js
+++ server/public/js/app.js
@@ -2505,98 +2505,6 @@ var averageRank = {
   1: 25,
   2: 43,
   3: 85
-};
-var bubleChartData = {
-  datasets: [{
-    // データ(0個目)
-    data: [{
-      "x": averageAge[0],
-      "y": averageRank[0],
-      "r": 20
-    }],
-    backgroundColor: lightBlue,
-    label: ["10s table"]
-  }, {
-    // データ(1個目)
-    data: [{
-      "x": averageAge[1],
-      "y": averageRank[1],
-      "r": 40
-    }],
-    backgroundColor: lightPurple,
-    label: ["20s table"]
-  }, {
-    // データ(2個目)
-    data: [{
-      "x": averageAge[2],
-      "y": averageRank[2],
-      "r": 50
-    }],
-    backgroundColor: lightRed,
-    label: ["30s table"]
-  }, {
-    // データ(3個目)
-    data: [{
-      "x": averageAge[3],
-      "y": averageRank[3],
-      "r": 30
-    }],
-    backgroundColor: lightGreen,
-    label: ["40s table"]
-  }]
-}; // オプション
-
-var options = {
-  title: {
-    display: true,
-    position: 'top',
-    fontSize: 17,
-    text: 'Generation Analysis *at latest ranking'
-  },
-  // スケール
-  scales: {
-    // x軸 : 年齢
-    xAxes: [{
-      ticks: {
-        min: 0,
-        max: 50,
-        stepSize: 10,
-        callback: function callback(value) {
-          return value + 's'; //labelに「〜位」とかをつけれる
-        }
-      },
-      // x軸のラベル
-      scaleLabel: {
-        display: true,
-        fontSize: 12,
-        labelString: "Age"
-      }
-    }],
-    // y軸 : ランキング
-    yAxes: [{
-      // y軸のラベル
-      scaleLabel: {
-        display: true,
-        fontSize: 12,
-        labelString: "Ranking"
-      },
-      ticks: {
-        reverse: true,
-        max: 100,
-        min: 1,
-        stepSize: 20
-      }
-    }]
-  },
-  // tooltip
-  tooltips: {
-    callbacks: {
-      label: function label(t, d) {
-        var rLabel = d.datasets[t.datasetIndex].data[t.index].r;
-        return d.datasets[t.datasetIndex].label + ': (x軸:' + t.xLabel + ', y軸:' + t.yLabel + ', 円の大きさ:' + rLabel + ')';
-      }
-    }
-  }
 }; // ここでチャートの種類を選択
 
 /**
@@ -2616,8 +2524,97 @@ var options = {
   name: 'Bubble',
   data: function data() {
     return {
-      chartdata: bubleChartData,
-      options: options
+      chartdata: {
+        datasets: [{
+          // データ(0個目)
+          data: [{
+            "x": averageAge[0],
+            "y": averageRank[0],
+            "r": 20
+          }],
+          backgroundColor: lightBlue,
+          label: ["10s table"]
+        }, {
+          // データ(1個目)
+          data: [{
+            "x": averageAge[1],
+            "y": averageRank[1],
+            "r": 40
+          }],
+          backgroundColor: lightPurple,
+          label: ["20s table"]
+        }, {
+          // データ(2個目)
+          data: [{
+            "x": averageAge[2],
+            "y": averageRank[2],
+            "r": 50
+          }],
+          backgroundColor: lightRed,
+          label: ["30s table"]
+        }, {
+          // データ(3個目)
+          data: [{
+            "x": averageAge[3],
+            "y": averageRank[3],
+            "r": 30
+          }],
+          backgroundColor: lightGreen,
+          label: ["40s table"]
+        }]
+      },
+      options: {
+        title: {
+          display: true,
+          position: 'top',
+          fontSize: 17,
+          text: 'Generation Analysis *at latest ranking'
+        },
+        // スケール
+        scales: {
+          // x軸 : 年齢
+          xAxes: [{
+            ticks: {
+              min: 0,
+              max: 50,
+              stepSize: 10,
+              callback: function callback(value) {
+                return value + 's'; //labelに「〜位」とかをつけれる
+              }
+            },
+            // x軸のラベル
+            scaleLabel: {
+              display: true,
+              fontSize: 12,
+              labelString: "Age"
+            }
+          }],
+          // y軸 : ランキング
+          yAxes: [{
+            // y軸のラベル
+            scaleLabel: {
+              display: true,
+              fontSize: 12,
+              labelString: "Ranking"
+            },
+            ticks: {
+              reverse: true,
+              max: 100,
+              min: 1,
+              stepSize: 20
+            }
+          }]
+        },
+        // tooltip
+        tooltips: {
+          callbacks: {
+            label: function label(t, d) {
+              var rLabel = d.datasets[t.datasetIndex].data[t.index].r;
+              return d.datasets[t.datasetIndex].label + ': (x軸:' + t.xLabel + ', y軸:' + t.yLabel + ', 円の大きさ:' + rLabel + ')';
+            }
+          }
+        }
+      }
     };
   },
   mounted: function mounted() {
--- server/resources/js/components/RankingChartComponent.vue
+++ server/resources/js/components/RankingChartComponent.vue
@@ -19,92 +19,6 @@ let averageRank = {
     3:85,
 };
 
-let bubleChartData = {
-    datasets: [
-        {
-            // データ(0個目)
-            data: [{"x":averageAge[0] ,"y":averageRank[0], "r":20} ,],
-            backgroundColor: lightBlue,
-            label: ["10s table"] 
-        },
-        {
-            // データ(1個目)
-            data: [{"x":averageAge[1] ,"y":averageRank[1], "r":40} ,],
-            backgroundColor: lightPurple,
-            label: ["20s table"] 
-        },
-        {
-            // データ(2個目)
-            data: [{"x":averageAge[2] , "y":averageRank[2], "r":50} ,],
-            backgroundColor: lightRed,
-            label: ["30s table"]  
-        },
-        {
-            // データ(3個目)
-            data: [{"x":averageAge[3] ,"y":averageRank[3], "r":30} ,],
-            backgroundColor: lightGreen,
-            label: ["40s table"]  
-        }
-    ]
-};
-
-
-  // オプション
-let options = {
-    title: {
-        display: true,
-        position:'top',
-        fontSize: 17,
-        text: 'Generation Analysis *at latest ranking'
-    },
-    // スケール
-    scales: {
-        // x軸 : 年齢
-        xAxes: [{
-            ticks: {
-                min: 0,
-                max: 50,
-                stepSize: 10,
-                callback: function(value){
-                    return value + 's';  //labelに「〜位」とかをつけれる
-                }
-            },
-            // x軸のラベル
-            scaleLabel: {
-                display: true,
-                fontSize: 12,
-                labelString: "Age",
-            },
-        }],
-        // y軸 : ランキング
-        yAxes: [{
-            // y軸のラベル
-            scaleLabel: {
-                display: true,
-                fontSize: 12,
-                labelString: "Ranking"
-            },
-            ticks: {
-                reverse: true,
-                max: 100,
-                min: 1,
-                stepSize: 20
-            }
-        }]
-    },
-    // tooltip
-    tooltips: {
-        callbacks: {
-            label: function(t, d) {
-            let rLabel = d.datasets[t.datasetIndex].data[t.index].r;
-            return d.datasets[t.datasetIndex].label + 
-                    ': (x軸:' + t.xLabel + ', y軸:' + t.yLabel + ', 円の大きさ:' + rLabel + ')';
-            }
-        }
-    }
-};
-
-
 // ここでチャートの種類を選択
 /**
  * Bar       : 棒グラフ
@@ -122,8 +36,87 @@ export default {
     extends: Bubble,
     name: 'Bubble',
     data:() => ({
-        chartdata: bubleChartData,
-        options: options,
+        chartdata: {
+            datasets: [
+                {
+                    // データ(0個目)
+                    data: [{"x":averageAge[0] ,"y":averageRank[0], "r":20} ,],
+                    backgroundColor: lightBlue,
+                    label: ["10s table"] 
+                },
+                {
+                    // データ(1個目)
+                    data: [{"x":averageAge[1] ,"y":averageRank[1], "r":40} ,],
+                    backgroundColor: lightPurple,
+                    label: ["20s table"] 
+                },
+                {
+                    // データ(2個目)
+                    data: [{"x":averageAge[2] , "y":averageRank[2], "r":50} ,],
+                    backgroundColor: lightRed,
+                    label: ["30s table"]  
+                },
+                {
+                    // データ(3個目)
+                    data: [{"x":averageAge[3] ,"y":averageRank[3], "r":30} ,],
+                    backgroundColor: lightGreen,
+                    label: ["40s table"]  
+                }
+            ]
+        },
+        options: {
+            title: {
+                display: true,
+                position:'top',
+                fontSize: 17,
+                text: 'Generation Analysis *at latest ranking'
+            },
+            // スケール
+            scales: {
+                // x軸 : 年齢
+                xAxes: [{
+                    ticks: {
+                        min: 0,
+                        max: 50,
+                        stepSize: 10,
+                        callback: function(value){
+                            return value + 's';  //labelに「〜位」とかをつけれる
+                        }
+                    },
+                    // x軸のラベル
+                    scaleLabel: {
+                        display: true,
+                        fontSize: 12,
+                        labelString: "Age",
+                    },
+                }],
+                // y軸 : ランキング
+                yAxes: [{
+                    // y軸のラベル
+                    scaleLabel: {
+                        display: true,
+                        fontSize: 12,
+                        labelString: "Ranking"
+                    },
+                    ticks: {
+                        reverse: true,
+                        max: 100,
+                        min: 1,
+                        stepSize: 20
+                    }
+                }]
+            },
+            // tooltip
+            tooltips: {
+                callbacks: {
+                    label: function(t, d) {
+                    let rLabel = d.datasets[t.datasetIndex].data[t.index].r;
+                    return d.datasets[t.datasetIndex].label + 
+                            ': (x軸:' + t.xLabel + ', y軸:' + t.yLabel + ', 円の大きさ:' + rLabel + ')';
+                    }
+                }
+            }
+        },
     }),
     mounted () {
         this.renderChart(this.chartdata, this.options);
