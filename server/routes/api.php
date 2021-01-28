2faecf40ce58fef9607fdd0f6778ec8987ea7250
0kkun <shinji.okumoto.01@gmail.com>
2021-01-28 20:19:05 +0900

    同一オリジンなのでルートをapiからwebに移動

--- server/routes/api.php
+++ server/routes/api.php
@@ -12,28 +12,3 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
-
-
-Route::middleware('throttle:60,1')->prefix('v1')->group(function () {
-    Route::get('/brands', 'FavoriteBrandController@fetchBrands');
-    Route::post('/brands/create', 'FavoriteBrandController@addBrand');
-    Route::delete('/brands/delete', 'FavoriteBrandController@deleteBrand');
-
-    Route::get('/rankings', 'RankingController@fetchRankings');
-    Route::get('/players_news', 'NewsController@fetchPlayersNews');
-    Route::get('/brands_news', 'NewsController@fetchBrandsNews');
-    Route::get('/player_movies', 'MovieController@fetchPlayerMovies');
-    Route::get('/brand_movies', 'MovieController@fetchBrandMovies');
-    Route::get('/tour_schedules', 'TourScheduleController@fetchTourSchedules');
-
-    Route::get('/players', 'FavoritePlayerController@fetchPlayers');
-    Route::post('/add_player', 'FavoritePlayerController@addPlayer');
-    Route::delete('/delete_player', 'FavoritePlayerController@deletePlayer');
-    Route::get('/search_players', 'FavoritePlayerController@searchPlayers');
-    Route::get('/analysis_age', 'AnalysisController@fetchAgeAnalysis');
-});
--- server/routes/web.php
+++ server/routes/web.php
@@ -25,3 +25,24 @@ Route::middleware('auth:web')->group(function () {
     Route::get('/favorite_brand', 'FavoriteBrandController@top')->name('favorite_brand.top');
     Route::get('/favorite_player', 'FavoritePlayerController@top')->name('favorite_player.top');
 });
+
+
+/* APIのルート */
+Route::middleware('throttle:60,1', 'auth:web')->prefix('/api/v1')->group(function () {
+    Route::get('/brands', 'FavoriteBrandController@fetchBrands');
+    Route::post('/brands/create', 'FavoriteBrandController@addBrand');
+    Route::delete('/brands/delete', 'FavoriteBrandController@deleteBrand');
+
+    Route::get('/rankings', 'RankingController@fetchRankings');
+    Route::get('/players_news', 'NewsController@fetchPlayersNews');
+    Route::get('/brands_news', 'NewsController@fetchBrandsNews');
+    Route::get('/player_movies', 'MovieController@fetchPlayerMovies');
+    Route::get('/brand_movies', 'MovieController@fetchBrandMovies');
+    Route::get('/tour_schedules', 'TourScheduleController@fetchTourSchedules');
+
+    Route::get('/players', 'FavoritePlayerController@fetchPlayers');
+    Route::post('/add_player', 'FavoritePlayerController@addPlayer');
+    Route::delete('/delete_player', 'FavoritePlayerController@deletePlayer');
+    Route::get('/search_players', 'FavoritePlayerController@searchPlayers');
+    Route::get('/analysis_age', 'AnalysisController@fetchAgeAnalysis');
+});
