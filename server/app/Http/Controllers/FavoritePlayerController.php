0c712fe4713ca6810f28dd81db5abecac8fb608e
0kkun <shinji.okumoto.01@gmail.com>
2021-02-03 09:27:57 +0900

    clear: FavoritePlayerControllerのリファクタ

--- server/app/Http/Controllers/FavoritePlayerController.php
+++ server/app/Http/Controllers/FavoritePlayerController.php
@@ -9,15 +9,15 @@ use App\Services\FavoritePlayer\FavoritePlayerServiceInterface;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Collection;
 use App\Services\Api\ApiServiceInterface;
-use App\Modules\BatchLogger;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
 
 class FavoritePlayerController extends Controller
 {
     private $players_repository;
     private $favorite_players_repository;
     private $api_service;
-    private $logger;
 
     // レスポンスのフォーマット
     protected $response;
@@ -38,7 +38,6 @@ class FavoritePlayerController extends Controller
         ApiServiceInterface $api_service
     )
     {
-        $this->logger = new BatchLogger(__CLASS__);
         $this->response = config('api_template.response_format');
         $this->result_status = config('api_template.result_status');
         $this->players_repository = $players_repository;
@@ -71,9 +70,12 @@ class FavoritePlayerController extends Controller
     public function fetchPlayers(Request $request): JsonResponse
     {
         try {
+            $start = microtime(true);
+            Log::info("[START] " . __FUNCTION__ );
+
             // リクエストの中身をチェック
-            $expected_key = ['user_id'];
-            $status = $this->api_service->checkArgs($request, $expected_key);
+            $is_varidation_error = $this->checkValidationError(__FUNCTION__, $request->all());
+            $status = $this->getStatusCode($is_varidation_error);
 
             if ($status === $this->result_status['success']) {
 
@@ -93,17 +95,14 @@ class FavoritePlayerController extends Controller
                 $this->response = ['status' => $status, 'data' => ''];
             }
 
-            $this->logger->write('status code :' . $status, 'info');
-            $this->logger->success();
-
+            $end = microtime(true);
+            $time = $this->api_service->calcTime($start, $end);
+            Log::info("[ END ] " . __FUNCTION__ . ", STATUS:" . $status . ", 処理時間:" . $time . "秒");
             return response()->json($this->response);
 
         } catch (\Exception $e) {
-            $this->logger->exception($e);
-            $status = $this->result_status['server_error'];
-            $error_info = $this->api_service->makeErrorInfo($e);
-            $this->response = ['status' => $status,'data' => $error_info];
-
+            Log::info("[Exception]" . __FUNCTION__ . $e->getMessage());
+            $this->respose = $this->api_service->makeErrorResponse($e);
             return response()->json($this->response);
         }
     }
@@ -118,9 +117,12 @@ class FavoritePlayerController extends Controller
     public function addPlayer(Request $request): JsonResponse
     {
         try {
+            $start = microtime(true);
+            Log::info("[START] " . __FUNCTION__ );
+
             // リクエストの中身をチェック
-            $expected_key = ['user_id', 'favorite_player_id'];
-            $status = $this->api_service->checkArgs($request, $expected_key);
+            $is_varidation_error = $this->checkValidationError(__FUNCTION__, $request->all());
+            $status = $this->getStatusCode($is_varidation_error);
 
             if ($status === $this->result_status['success']) {
 
@@ -137,14 +139,14 @@ class FavoritePlayerController extends Controller
                 $this->response = ['status' => $status, 'data' => ''];
             }
 
+            $end = microtime(true);
+            $time = $this->api_service->calcTime($start, $end);
+            Log::info("[ END ] " . __FUNCTION__ . ", STATUS:" . $status . ", 処理時間:" . $time . "秒");
             return response()->json($this->response);
 
         } catch (\Exception $e) {
-            $this->logger->exception($e);
-            $status = $this->result_status['server_error'];
-            $error_info = $this->api_service->makeErrorInfo($e);
-            $this->response = ['status' => $status,'data' => $error_info];
-
+            Log::info("[Exception]" . __FUNCTION__ . $e->getMessage());
+            $this->respose = $this->api_service->makeErrorResponse($e);
             return response()->json($this->response);
         }
     }
@@ -159,9 +161,12 @@ class FavoritePlayerController extends Controller
     public function deletePlayer(Request $request): JsonResponse
     {
         try {
+            $start = microtime(true);
+            Log::info("[START] " . __FUNCTION__ );
+
             // リクエストの中身をチェック
-            $expected_key = ['user_id', 'favorite_player_id'];
-            $status = $this->api_service->checkArgs($request, $expected_key);
+            $is_varidation_error = $this->checkValidationError(__FUNCTION__, $request->all());
+            $status = $this->getStatusCode($is_varidation_error);
 
             if ($status === $this->result_status['success']) {
                 $data['user_id'] = $request->input('user_id');
@@ -177,14 +182,14 @@ class FavoritePlayerController extends Controller
                 $this->response = ['status' => $status, 'data' => ''];
             }
 
+            $end = microtime(true);
+            $time = $this->api_service->calcTime($start, $end);
+            Log::info("[ END ] " . __FUNCTION__ . ", STATUS:" . $status . ", 処理時間:" . $time . "秒");
             return response()->json($this->response);
 
         } catch (\Exception $e) {
-            $this->logger->exception($e);
-            $status = $this->result_status['server_error'];
-            $error_info = $this->api_service->makeErrorInfo($e);
-            $this->response = ['status' => $status,'data' => $error_info];
-
+            Log::info("[Exception]" . __FUNCTION__ . $e->getMessage());
+            $this->respose = $this->api_service->makeErrorResponse($e);
             return response()->json($this->response);
         }
     }
@@ -199,9 +204,12 @@ class FavoritePlayerController extends Controller
     public function searchPlayers(Request $request): JsonResponse
     {
         try {
+            $start = microtime(true);
+            Log::info("[START] " . __FUNCTION__ );
+
             // リクエストの中身をチェック
-            $expected_key = ['keyword', 'user_id'];
-            $status = $this->api_service->checkArgs($request, $expected_key);
+            $is_varidation_error = $this->checkValidationError(__FUNCTION__, $request->all());
+            $status = $this->getStatusCode($is_varidation_error);
 
             if ($status === $this->result_status['success']) {
 
@@ -221,16 +229,62 @@ class FavoritePlayerController extends Controller
                 $this->response = ['status' => $status, 'data' => ''];
             }
 
+            $end = microtime(true);
+            $time = $this->api_service->calcTime($start, $end);
+            Log::info("[ END ] " . __FUNCTION__ . ", STATUS:" . $status . ", 処理時間:" . $time . "秒");
             return response()->json($this->response);
 
         } catch (\Exception $e) {
-            $this->logger->exception($e);
-            $status = $this->result_status['server_error'];
-            $error_info = $this->api_service->makeErrorInfo($e);
-            $this->response = ['status' => $status,'data' => $error_info];
-
+            Log::info("[Exception]" . __FUNCTION__ . $e->getMessage());
+            $this->respose = $this->api_service->makeErrorResponse($e);
             return response()->json($this->response);
         }
+    }
+
+
+    /**
+     * バリデーションエラーか判定する
+     *
+     * @param string $func_name
+     * @param array $check_keys
+     * @return boolean
+     */
+    private function checkValidationError(string $func_name, array $check_keys): bool
+    {
+        $func_and_keys_pattern = [
+            'fetchPlayers' => [
+                'user_id' => 'required|integer'
+            ],
+            'addPlayer' => [
+                'user_id'            => 'required|integer',
+                'favorite_player_id' => 'required|integer',
+            ],
+            'deletePlayer' => [
+                'user_id'            => 'required|integer',
+                'favorite_player_id' => 'required|integer',
+            ],
+            'searchPlayers' => [
+                'keyword' => 'required|string|between:1,100',
+                'user_id' => 'required|integer',
+            ],
+        ];
+        $validator = Validator::make($check_keys, $func_and_keys_pattern[$func_name]);
+
+        $is_validation_error = !empty($validator->errors()->messages());
+
+        return $is_validation_error;
+    }
+
+
+    /**
+     * バリデーションチェックの結果に基づくステータスコードを取得
+     *
+     * @param boolean $is_validation_error
+     * @return integer
+     */
+    private function getStatusCode(bool $is_validation_error): int
+    {
+        return $is_validation_error ? $this->result_status['bad_request'] : $this->result_status['success'];
     }
 
 
