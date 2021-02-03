f540aa63937f356c45c979ee1eef6d0fa687e95a
0kkun <shinji.okumoto.01@gmail.com>
2021-02-04 08:52:09 +0900

    update: frontのテストケース追加

--- server/tests/Feature/Fronts/FrontAccessTest.php
+++ server/tests/Feature/Fronts/FrontAccessTest.php
@@ -15,7 +15,6 @@ class FrontAccessTest extends TestCase
         $this->user = $this->makeTestUser();
     }
 
-
     protected function tearDown()
     {
         parent::tearDown();
@@ -40,7 +39,8 @@ class FrontAccessTest extends TestCase
     {
         $this->withoutExceptionHandling();
         $response = $this->get(route('top.index'));
-        $response->assertStatus(200);
+        $response->assertStatus(200)
+            ->assertViewIs('top.index');
     }
 
     /**
@@ -58,12 +58,30 @@ class FrontAccessTest extends TestCase
     /**
      * @test
      */
+    public function Front_home_ログイン無しアクセスした場合、認証エラーステータスになるか()
+    {
+        $response = $this->get(route('home.index'));
+        $response->assertStatus(302);
+    }
+
+    /**
+     * @test
+     */
     public function Front_newsにアクセスできるか()
     {
         $this->withoutExceptionHandling();
         $response = $this->actingAs($this->user)->get(route('news.top'));
         $response->assertStatus(200)
             ->assertViewIs('news.top');
+    }
+
+    /**
+     * @test
+     */
+    public function Front_news_ログイン無しアクセスした場合、認証エラーステータスになるか()
+    {
+        $response = $this->get(route('news.top'));
+        $response->assertStatus(302);
     }
 
     /**
@@ -80,6 +98,15 @@ class FrontAccessTest extends TestCase
     /**
      * @test
      */
+    public function Front_ranking_ログイン無しアクセスした場合、認証エラーステータスになるか()
+    {
+        $response = $this->get(route('ranking.top'));
+        $response->assertStatus(302);
+    }
+
+    /**
+     * @test
+     */
     public function Front_favorite_brandにアクセスできるか()
     {
         $this->withoutExceptionHandling();
@@ -91,12 +118,30 @@ class FrontAccessTest extends TestCase
     /**
      * @test
      */
+    public function Front_favorite_brand_ログイン無しアクセスした場合、認証エラーステータスになるか()
+    {
+        $response = $this->get(route('favorite_brand.top'));
+        $response->assertStatus(302);
+    }
+
+    /**
+     * @test
+     */
     public function Front_favorite_playerにアクセスできるか()
     {
         $this->withoutExceptionHandling();
         $response = $this->actingAs($this->user)->get(route('favorite_player.top'));
         $response->assertStatus(200)
             ->assertViewIs('favorite_player.top');
+    }
+
+    /**
+     * @test
+     */
+    public function Front_favorite_player_ログイン無しアクセスした場合、認証エラーステータスになるか()
+    {
+        $response = $this->get(route('favorite_player.top'));
+        $response->assertStatus(302);
     }
 
     /**
